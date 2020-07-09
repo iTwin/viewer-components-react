eac198b895ad19407ca965811daead5277b93b99
Archana Maharjan <52217155+archana-maharjan@users.noreply.github.com>
2020-07-09 15:18:17 -0500

    test not working but setting up test

--- packages/markup-frontstage/src/tests/components/toolbar/MarkupSettings.test.tsx
+++ packages/markup-frontstage/src/tests/components/toolbar/MarkupSettings.test.tsx
@@ -1,16 +1,16 @@
 import { Point, UiCore } from "@bentley/ui-core";
 import { render } from "@testing-library/react";
 import React from "react";
-import { MockService } from "../../util/MockService";
+// import { MockService } from "../../util/MockService";
 
 import MarkupSettingsPanel from "../../../components/toolbar/MarkupSettings";
 import { I18N } from "@bentley/imodeljs-i18n";
 
-// jest.mock("@bentley/ui-abstract");
-// jest.mock("@bentley/ui-components");
-// jest.mock("@bentley/ui-core");
-// jest.mock("@bentley/ui-framework");
-// jest.mock("@bentley/imodeljs-frontend");
+jest.mock("@bentley/ui-abstract");
+jest.mock("@bentley/ui-components");
+jest.mock("@bentley/ui-core");
+jest.mock("@bentley/ui-framework");
+jest.mock("@bentley/imodeljs-frontend");
 
 describe("MarkupSettings tool testing", () => {
   it("should render", async () => {
