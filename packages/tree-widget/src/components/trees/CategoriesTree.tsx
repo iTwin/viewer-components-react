cfdb0d5b3989b1fb2b0784e7f2d334b5587faf0a
Zach Zoltek <zjzoltek@users.noreply.github.com>
2020-07-13 16:29:57 -0500

    changing copyright headers again

--- packages/tree-widget/src/components/trees/CategoriesTree.tsx
+++ packages/tree-widget/src/components/trees/CategoriesTree.tsx
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
-right (c) Bentley Systems, Incorporated. All rights reserved.
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
