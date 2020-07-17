cf08e1715c68a07cdb6b8c767192f4041f41563f
Archana Maharjan <52217155+archana-maharjan@users.noreply.github.com>
2020-07-17 18:00:11 -0500

    Update readme (#20)
    Updated readme to add markup-package folder, updated Repository url of markup-package, added MarkupFrontstage export in react.

--- packages/markup-frontstage/src/markup-frontstage-react.ts
+++ packages/markup-frontstage/src/markup-frontstage-react.ts
@@ -2,6 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+export * from "./MarkupFrontstage";
 export * from "./components/ui/MarkupFrontstageProvider";
 export * from "./components/toolbar/MarkupSettingsPanel";
 export * from "./util/MarkupTypes";
