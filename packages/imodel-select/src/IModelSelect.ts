04ec8b6fd75b8ce83156094f336c45f30b7504be
mdastous-bentley <mdastous-bentley@users.noreply.github.com>
2020-11-17 13:31:30 -0500

    Revert changes to imodelselect

--- packages/imodel-select/src/IModelSelect.ts
+++ packages/imodel-select/src/IModelSelect.ts
@@ -7,7 +7,7 @@
 
 import * as i18next from "i18next";
 import { I18N } from "@bentley/imodeljs-i18n";
-import { getClassName, UiError } from "@bentley/ui-abstract";
+import { UiError, getClassName } from "@bentley/ui-abstract";
 
 /**
  * Entry point for static initialization required by various components used in the package.
