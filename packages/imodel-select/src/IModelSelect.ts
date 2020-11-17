b4564b1a5a2c734afc2cce405817c117cc798acf
mdastous-bentley <mdastous-bentley@users.noreply.github.com>
2020-11-17 14:49:01 -0500

    Reverted ImodelSelect.ts

--- packages/imodel-select/src/IModelSelect.ts
+++ packages/imodel-select/src/IModelSelect.ts
@@ -2,7 +2,6 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-
 /** @module Common */
 
 import * as i18next from "i18next";
