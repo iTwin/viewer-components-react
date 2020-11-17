4d2fbe42b068cbb442d4686779caf9bee34fa079
mdastous-bentley <mdastous-bentley@users.noreply.github.com>
2020-11-17 14:37:00 -0500

    Rerse changes to IModelSelect (Again)

--- packages/imodel-select/src/IModelSelect.ts
+++ packages/imodel-select/src/IModelSelect.ts
@@ -2,6 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+
 /** @module Common */
 
 import * as i18next from "i18next";
