52b3dc41adc94c6a96cdb774ef34ed545fcb6eac
Archana Maharjan <52217155+archana-maharjan@users.noreply.github.com>
2021-02-18 10:15:57 -0600

    feat: Add thumbnail size options to allow users to get bigger thumbnails than 360

--- packages/markup-frontstage/src/util/MarkupViewStateAdapter.ts
+++ packages/markup-frontstage/src/util/MarkupViewStateAdapter.ts
@@ -3,10 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import {
-  Code,
-  SheetProps,
-} from "@bentley/imodeljs-common";
+
 import {
   DrawingViewState,
   EmphasizeElements,
@@ -25,32 +22,11 @@ import { MarkupViewStateProps } from "./MarkupViewState";
  */
 export const createViewStateProps = (vp: Viewport, markup: string): MarkupViewStateProps => {
   const viewState = vp.view.isSpatialView() ? vp.view as SpatialViewState : vp.view.isDrawingView() ? vp.view as DrawingViewState : vp.view as SheetViewState;
-  const categorySelectorProps = viewState.categorySelector?.toJSON();
-  const displayStyleProps = viewState.displayStyle?.toJSON();
+  const props = viewState.toProps();
   const emphasizedElementsProps = EmphasizeElements.get(vp)?.toJSON(vp);
-  const modelExtents = viewState.getViewedExtents().toJSON();
-  const viewDefinitionProps = viewState.toJSON();
   const markupViewStateProps: MarkupViewStateProps = {
-    categorySelectorProps,
-    displayStyleProps,
+    ...props,
     emphasizedElementsProps,
-    modelExtents,
-    viewDefinitionProps: viewDefinitionProps,
-  }
-  if (vp.view.isSpatialView()) {
-    markupViewStateProps.modelSelectorProps = (viewState as SpatialViewState).modelSelector.toJSON();
-  }
-  if (viewState instanceof SheetViewState) {
-    const sheetSize = viewState.sheetSize;
-    const sheetProps: SheetProps = {
-      width: sheetSize.x,
-      height: sheetSize.y,
-      model: viewState.model,
-      classFullName: SheetViewState.classFullName,
-      code: Code.createEmpty(),
-    };
-    markupViewStateProps.sheetAttachments = viewState.attachmentIds;
-    markupViewStateProps.sheetProps = sheetProps;
   }
   if (markup !== undefined) {
     markupViewStateProps.markup = markup;
