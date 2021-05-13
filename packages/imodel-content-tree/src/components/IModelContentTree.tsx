95f172663471d80326ffc3be97a1dacc0169f54a
Jason Crow <25822334+jason-crow@users.noreply.github.com>
2021-05-13 15:57:05 -0500

    update tree-widget to 2.15.2

--- packages/imodel-content-tree/src/components/IModelContentTree.tsx
+++ packages/imodel-content-tree/src/components/IModelContentTree.tsx
@@ -3,6 +3,7 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
+
 import "./IModelContentTree.scss";
 import classNames from "classnames";
 import * as React from "react";
@@ -12,7 +13,9 @@ import { usePresentationTreeNodeLoader } from "@bentley/presentation-components"
 import { ControlledTree, SelectionMode, useTreeEventsHandler, useVisibleTreeNodes } from "@bentley/ui-components";
 import RULESET_TREE_HIERARCHY from "./Hierarchy.json";
 
-/** Props for the [[IModelContentTree]] component */
+/**
+ * Props for the [[IModelContentTree]] component
+ */
 export interface IModelContentTreeProps extends Omit<React.AllHTMLAttributes<HTMLDivElement>, "children"> {
   iModel: IModelConnection;
 }
