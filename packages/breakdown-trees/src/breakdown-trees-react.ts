7c20dda5d8cc09b770e11cabee0ae98b3c5b2e03
MeghaCBorty <88436360+MeghaCBorty@users.noreply.github.com>
2021-11-12 20:59:53 +0530

    Export data trees (#149)

--- packages/breakdown-trees/src/breakdown-trees-react.ts
+++ packages/breakdown-trees/src/breakdown-trees-react.ts
@@ -3,3 +3,9 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 export * from "./BreakdownTrees";
+export * from "./Views/SpatialContainmentTree";
+export * from "./Views/SpatialContainmentTree.SessionStorage";
+export * from "./Views/ClassificationsTree";
+export * from "./Views/ClassificationsTree.SessionStorage";
+export * from "./Views/ComponentIndex";
+export * from "./Views/ComponentIndex.SessionStorage";
