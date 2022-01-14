1f12c169522ee3a88bf6b64c7b75e69e56f02365
Arnob Mallick <arnobmallickbsw@users.noreply.github.com>
2022-01-14 00:04:17 +0000

    remove prettier in favor of common eslint rules

--- packages/itwin/grouping-mapping-widget/.eslintrc.js
+++ packages/itwin/grouping-mapping-widget/.eslintrc.js
@@ -20,12 +20,9 @@ module.exports = {
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
     "plugin:react-hooks/recommended",
-    "prettier",
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    "prettier/prettier": [1],
     "react/prop-types": "off",
     "react/function-component-definition": [
       1,
