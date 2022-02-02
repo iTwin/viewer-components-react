9bc0925dcb00a41119fea9ae9d01cbffc5f772a9
Guillar1 <Guillar1@users.noreply.github.com>
2022-02-02 14:50:09 -0800

    remove usefetchaccesstoken hook

--- packages/itwin/grouping-mapping-widget/src/widget/components/SelectIModel.tsx
+++ packages/itwin/grouping-mapping-widget/src/widget/components/SelectIModel.tsx
@@ -2,24 +2,41 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+import { IModelApp } from "@itwin/core-frontend";
 import type {
   ApiOverrides,
-  IModelFull} from "@itwin/imodel-browser-react";
+  IModelFull
+} from "@itwin/imodel-browser-react";
 import {
   IModelGrid,
 } from "@itwin/imodel-browser-react";
 import { Button } from "@itwin/itwinui-react";
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { getUrlPrefix } from "../../api/reportingClient";
-import useFetchAccessToken from "../hooks/useFetchAccessToken";
 import "./SelectIModel.scss";
 
+
+
+const useFetchAccessToken = () => {
+  const [accessToken, setAccessToken] = useState<string>("");
+
+  useEffect(() => {
+    const fetchAccessToken = async () => {
+      const accessToken = await IModelApp.authorizationClient?.getAccessToken();
+      setAccessToken(accessToken ?? "");
+    };
+    void fetchAccessToken();
+  }, []);
+
+  return accessToken;
+};
 interface SelectIModelProps {
   projectId: string;
   onSelect: (project: IModelFull) => void;
   onCancel: () => void;
   backFn: () => void;
 }
+
 const SelectIModel = ({
   projectId,
   onSelect,
