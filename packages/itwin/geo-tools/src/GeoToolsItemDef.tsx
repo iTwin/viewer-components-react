6246a775545b47c4cc326697815a57141d832621
Arun George <11051042+aruniverse@users.noreply.github.com>
2022-02-16 16:41:22 -0500

    [GeoTools] remove svg?sprite and add ui provider (#192)
    Co-authored-by: Arun George <aruniverse@users.noreply.github.com>

--- packages/itwin/geo-tools/src/GeoToolsItemDef.tsx
+++ packages/itwin/geo-tools/src/GeoToolsItemDef.tsx
@@ -11,27 +11,23 @@ import * as React from "react";
 import { ToolbarPopupContext } from "@itwin/components-react";
 import { CustomItemDef } from "@itwin/appui-react";
 import { GeoAddressSearch } from "./components/GeoAddressSearch";
-import geoSearchSvg from "./icons/geosearch.svg?sprite";
-import { IconSpecUtilities } from "@itwin/appui-abstract";
+import { SvgGeosearch } from "@itwin/itwinui-icons-react";
 
 /** Utility Class that provides definitions of tools. These definitions can be used to populate the UI.
  * @public
  */
 // istanbul ignore next
 export class GeoToolsItemDef {
-  public static iconSpec = IconSpecUtilities.createSvgIconSpec(geoSearchSvg);
-
   public static get geoAddressSearchButtonItemDef() {
     return new CustomItemDef({
       customId: "geo-tools:geoAddressSearch",
-      iconSpec: GeoToolsItemDef.iconSpec,
+      iconSpec: <SvgGeosearch />,
       labelKey: "GeoTools:geoAddressSearch.label",
-      popupPanelNode: <ToolbarPopupContext.Consumer>
-        {() => (
-          <GeoAddressSearch />
-        )}
-      </ToolbarPopupContext.Consumer>,
-
+      popupPanelNode: (
+        <ToolbarPopupContext.Consumer>
+          {() => <GeoAddressSearch />}
+        </ToolbarPopupContext.Consumer>
+      ),
     });
   }
 }
