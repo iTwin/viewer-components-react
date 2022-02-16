64de26a7601e4d7d0bf884e1a0774082a0d09b58
Arun George <aruniverse@users.noreply.github.com>
2022-02-16 13:27:31 -0500

    remove svg?sprite and add ui provider

--- packages/itwin/geo-tools/src/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-// Allows svg to be imported like this this:
-// import geoSearchSvg from "./icons/geosearch.svg?sprite";
-declare module "*.svg?sprite" {
-
-  const src: string;
-
-  export default src;
-
-}
