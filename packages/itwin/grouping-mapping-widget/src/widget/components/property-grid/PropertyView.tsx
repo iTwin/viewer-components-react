e5154e071dd1704497ba404d330f87f2641fb649
Guillar1 <Guillar1@users.noreply.github.com>
2022-03-11 15:50:04 -0800

    fixed checkbox indent and failed field fetch

--- packages/itwin/grouping-mapping-widget/src/widget/components/property-grid/PropertyView.tsx
+++ packages/itwin/grouping-mapping-widget/src/widget/components/property-grid/PropertyView.tsx
@@ -14,6 +14,7 @@ import { GroupQueryBuilderContext } from "../GroupQueryBuilderContext";
 import { useCallback } from "react";
 import { PropertyGridColumnStyleProvider } from "@itwin/components-react/lib/cjs/components-react/properties/renderers/PropertyGridColumns";
 import { Checkbox } from "@itwin/itwinui-react";
+import { CommonPropertyRenderer } from "@itwin/components-react/lib/cjs/components-react/properties/renderers/CommonPropertyRenderer";
 
 /** Properties of [[PropertyView]] React component
  * @public
@@ -25,6 +26,8 @@ export interface PropertyViewProps extends SharedRendererProps {
   valueElement?: React.ReactNode;
   /** Render callback for property value. If specified, `valueElement` is ignored. */
   valueElementRenderer?: () => React.ReactNode;
+  /** Multiplier of how much the property is indented to the right */
+  indentation?: number;
 }
 
 /**
@@ -93,13 +96,17 @@ export const PropertyView = (props: PropertyViewProps) => {
         !context.currentPropertyList.includes(prop) &&
         prop.value.valueFormat === PropertyValueFormat.Primitive
       ) {
-        if (prop.value.displayValue) {
+        if (prop.value.value === undefined) {
           setIsCheckboxLoading(true);
+        }
+        if (!(await context.queryBuilder.addProperty(prop))) {
+          setIsCheckboxLoading(false);
+          setIsPropertySelected(false);
+          return;
         }
         context.setCurrentPropertyList(
           context.currentPropertyList.concat(prop)
         );
-        await context.queryBuilder.addProperty(prop);
         context.setQuery(context.queryBuilder.buildQueryString());
       }
     },
@@ -189,16 +196,6 @@ export const PropertyView = (props: PropertyViewProps) => {
   ]);
 
   React.useEffect(() => {
-    if (props?.propertyRecord) {
-      if (context.currentPropertyList.includes(props.propertyRecord)) {
-        setIsPropertySelected(true);
-      } else {
-        setIsPropertySelected(false);
-      }
-    }
-  }, [context.currentPropertyList, props.propertyRecord]);
-
-  React.useEffect(() => {
     if (!context.isRendering)
       setIsCheckboxLoading(false);
   }, [context.isRendering]);
@@ -262,6 +259,14 @@ export const PropertyView = (props: PropertyViewProps) => {
     props.columnInfo
   );
 
+  const offset = CommonPropertyRenderer.getLabelOffset(
+    props.indentation,
+    props.orientation,
+    props.width,
+    props.columnRatio,
+    props.columnInfo?.minLabelWidth,
+  );
+
   return (
     <div
       style={columnsStyleProvider.getStyle(
@@ -281,10 +286,11 @@ export const PropertyView = (props: PropertyViewProps) => {
         {props.propertyRecord.value.valueFormat ===
           PropertyValueFormat.Primitive && (
           <Checkbox
+            style={{ marginLeft: offset }}
             className='components-property-selection-checkbox'
             checked={isPropertySelected}
             onChange={_onPropertySelectionChanged}
-            disabled={context.isLoading || context.isRendering}
+            disabled={context.isLoading || context.isRendering || (props.propertyRecord.value.value === undefined)}
             isLoading={isCheckboxLoading}
           />
         )}
