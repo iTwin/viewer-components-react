45a4cd63fdc81091dc7bae5d4c97b757cf8577b1
Arun George <aruniverse@users.noreply.github.com>
2022-03-17 13:05:59 -0400

    revert

--- packages/itwin/imodel-react-hooks/src/utils/react-context.ts
+++ packages/itwin/imodel-react-hooks/src/utils/react-context.ts
@@ -1,7 +1,8 @@
 /*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
 
 import React from "react";
 
@@ -9,7 +10,9 @@ import React from "react";
  * like React.createContext, but the default state (the one used when no provider is found by a consumer),
  * throws errors on usage, useful for when consuming without a provider should be considered programmer error.
  */
-export const makeContextWithProviderRequired = <T>(name = "UntitledContext") =>
+export const makeContextWithProviderRequired = <T extends any>(
+  name = "UntitledContext"
+) =>
   React.createContext<T>(
     new Proxy(
       {},
