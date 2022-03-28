d4c86711e246f529f77a61e5b5cf966f07833a61
bingjiez <bingjiez@users.noreply.github.com>
2022-03-28 16:17:39 -0700

    more unit tests

--- packages/itwin/one-click-lca-widget/src/test/WidgetHeader.test.tsx
+++ packages/itwin/one-click-lca-widget/src/test/WidgetHeader.test.tsx
@@ -3,12 +3,47 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { WidgetHeader } from "../components/utils";
 
 describe("Widget Header Component", function () {
   it("should have hello as title", function () {
-    let { getByText } = render(<WidgetHeader title="hello" />);
-    expect(getByText("hello")).toBeDefined();
+    const { container } = render(<WidgetHeader title="hello" />);
+    expect(screen.getByText("hello")).toBeDefined();
+    expect(container.firstChild).toHaveClass("oclca-widget-header-container");
+    expect(container.firstChild?.firstChild).toHaveClass("oclca-title");
+  });
+
+  it("with return function", function () {
+    const { container } = render(
+      <WidgetHeader title="hello" returnFn={async () => {}} />
+    );
+    expect(container.firstChild?.firstChild).toHaveClass("oclca-chevron");
+    expect(container.firstChild?.firstChild).not.toHaveClass(
+      "oclca-chevron-disabled"
+    );
+  });
+
+  it("with diabled", function () {
+    const { container } = render(
+      <WidgetHeader title="hello" disabled={true} returnFn={async () => {}} />
+    );
+    expect(container.firstChild?.firstChild).not.toHaveClass("oclca-chevron");
+    expect(container.firstChild?.firstChild).toHaveClass(
+      "oclca-chevron-disabled"
+    );
+  });
+
+  it("with mocked return function", function () {
+    const mockReturnFn = jest.fn();
+    const { container } = render(
+      <WidgetHeader title="hello" returnFn={mockReturnFn} />
+    );
+
+    const toTestElement = container.querySelector("SvgChevronLeft");
+    if (toTestElement !== null) {
+      fireEvent.click(toTestElement);
+    }
+    expect(mockReturnFn).toHaveBeenCalled;
   });
 });
