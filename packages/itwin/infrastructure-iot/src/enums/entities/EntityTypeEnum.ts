5dea3fd99bd46b8579b0fdecb355f3993f259009
Anton Ivanov <anton.aleks.ivanov@gmail.com>
2022-06-01 11:17:15 -0700

    [infrastructure-iot] Initial release of the infrastructure-iot package (#233)
    Co-authored-by: Anton Ivanov <10566684+antonai@users.noreply.github.com>
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>

---
+++ packages/itwin/infrastructure-iot/src/enums/entities/EntityTypeEnum.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+
+export enum EntityType {
+  NODE = "node",
+  DEVICE = "device",
+  SENSOR = "sensor"
+}
