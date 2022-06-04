be7a2590382aee8bc1bcac332ea1122c6c1c2fc3
Guillar1 <Guillar1@users.noreply.github.com>
2022-06-03 21:49:01 -0700

    i18n to localization

--- packages/itwin/reports-config-widget/src/ReportsConfigWidget.ts
+++ packages/itwin/reports-config-widget/src/ReportsConfigWidget.ts
@@ -7,29 +7,29 @@ import type { Localization } from "@itwin/core-common";
 import { IModelApp } from "@itwin/core-frontend";
 
 export class ReportsConfigWidget {
-  private static _i18nNamespace: string;
-  private static _i18n?: Localization;
+  private static _localizationNamespace: string;
+  private static _localization?: Localization;
 
-  public static get i18nNamespace(): any {
-    return ReportsConfigWidget._i18nNamespace;
+  public static get localizationNamespace(): string {
+    return ReportsConfigWidget._localizationNamespace;
   }
 
   public static async initialize(localization?: Localization): Promise<void> {
-    ReportsConfigWidget._i18n = localization;
-    // Setup i18n
+    ReportsConfigWidget._localization = localization ?? IModelApp.localization;
+    // Setup localization
     const ReportsWidgetNamespace = "ReportsConfigWidget";
-    if (ReportsConfigWidget._i18n) {
-      await ReportsConfigWidget._i18n.registerNamespace(ReportsWidgetNamespace);
+    if (ReportsConfigWidget._localization) {
+      await ReportsConfigWidget._localization.registerNamespace(ReportsWidgetNamespace);
     } else {
       await IModelApp.localization.registerNamespace(ReportsWidgetNamespace);
     }
 
-    ReportsConfigWidget._i18nNamespace = ReportsWidgetNamespace;
+    ReportsConfigWidget._localizationNamespace = ReportsWidgetNamespace;
   }
 
   public static terminate() {
-    if (ReportsConfigWidget._i18n)
-      ReportsConfigWidget._i18n.unregisterNamespace(ReportsConfigWidget.i18nNamespace);
-    ReportsConfigWidget._i18n = undefined;
+    if (ReportsConfigWidget._localization)
+      ReportsConfigWidget._localization.unregisterNamespace(ReportsConfigWidget.localizationNamespace);
+    ReportsConfigWidget._localization = undefined;
   }
 }
