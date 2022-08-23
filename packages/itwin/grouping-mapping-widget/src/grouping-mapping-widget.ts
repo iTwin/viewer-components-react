9cceee51c4b6917841a1b00feaa6f5e6b463ec03
Guillar1 <Guillar1@users.noreply.github.com>
2022-08-23 16:50:04 -0700

    Update grouping-mapping-widget.ts

--- packages/itwin/grouping-mapping-widget/src/grouping-mapping-widget.ts
+++ packages/itwin/grouping-mapping-widget/src/grouping-mapping-widget.ts
@@ -7,7 +7,7 @@ export * from "./widget/GroupingMappingWidget";
 
 /** Interfaces for providing custom MappingClient and API configuration */
 export { createDefaultMappingClient } from "./widget/components/context/MappingClientContext";
-export { ClientPrefix, GetAccessTokenFn} from "./widget/components/context/GroupingApiConfigContext";
+export { ClientPrefix, GetAccessTokenFn } from "./widget/components/context/GroupingApiConfigContext";
 export * from "@itwin/insights-client";
 export * from "./widget/IMappingClient";
 
