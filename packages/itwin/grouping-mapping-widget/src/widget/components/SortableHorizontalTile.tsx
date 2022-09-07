2c6c1f93886c12ad5ac38bfa95de2e50f6414cd3
Guillar1 <Guillar1@users.noreply.github.com>
2022-09-07 09:34:34 -0700

    copyright header

--- packages/itwin/grouping-mapping-widget/src/widget/components/SortableHorizontalTile.tsx
+++ packages/itwin/grouping-mapping-widget/src/widget/components/SortableHorizontalTile.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import React, { useEffect } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import type { HorizontalTileProps } from "./HorizontalTile";
