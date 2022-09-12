362cd1f984f19ed92e9803318e98a57d9a0e1e3b
Jay Patel <54871480+pateljay1397@users.noreply.github.com>
2022-09-12 15:41:27 -0400

    Setup DefaultTree for TreeView: (#321)
    Co-authored-by: Arun George <11051042+aruniverse@users.noreply.github.com>

--- packages/itwin/tree-widget/src/components/TreeWidgetUiItemsProvider.tsx
+++ packages/itwin/tree-widget/src/components/TreeWidgetUiItemsProvider.tsx
@@ -24,6 +24,7 @@ import { SpatialTreeComponent } from "./trees/SpatialTree";
 import type { SelectableContentDefinition } from "@itwin/components-react";
 import { TreeWidget } from "../TreeWidget";
 import type { TreeWidgetOptions } from "../types";
+import { CategoriesTreeId, ModelsTreeId, SpatialContainmentTreeId } from "../types";
 
 export const TreeWidgetId = "tree-widget-react:trees";
 export class TreeWidgetUiItemsProvider implements UiItemsProvider {
@@ -52,8 +53,8 @@ export class TreeWidgetUiItemsProvider implements UiItemsProvider {
 
       if (!this._treeWidgetOptions?.hideTrees?.modelsTree) {
         trees.push({
-          label: TreeWidget.translate("modeltree"),
-          id: "model-tree",
+          label: TreeWidget.translate("models"),
+          id: ModelsTreeId,
           render: () => (
             <ModelsTreeComponent
               enableElementsClassGrouping={
@@ -70,7 +71,7 @@ export class TreeWidgetUiItemsProvider implements UiItemsProvider {
       if (!this._treeWidgetOptions?.hideTrees?.categoriesTree) {
         trees.push({
           label: TreeWidget.translate("categories"),
-          id: "categories-tree",
+          id: CategoriesTreeId,
           render: () => (
             <CategoriesTreeComponent {...this._treeWidgetOptions?.categoriesTreeProps} />
           ),
@@ -80,7 +81,7 @@ export class TreeWidgetUiItemsProvider implements UiItemsProvider {
       if (!this._treeWidgetOptions?.hideTrees?.spatialTree) {
         trees.push({
           label: TreeWidget.translate("containment"),
-          id: "spatial-containment-tree",
+          id: SpatialContainmentTreeId,
           render: () => (
             <SpatialTreeComponent
               enableElementsClassGrouping={
@@ -96,6 +97,14 @@ export class TreeWidgetUiItemsProvider implements UiItemsProvider {
 
       if (this._treeWidgetOptions?.additionalTrees) {
         trees.push(...this._treeWidgetOptions.additionalTrees);
+      }
+
+      if(this._treeWidgetOptions?.defaultTreeId && trees.length !== 0){
+        // Adding the defaultTree to first index
+        const { defaultTreeId } = this._treeWidgetOptions;
+        const extractedDefaultTree = trees.filter((tree) => tree.id===defaultTreeId)[0];
+        const index = trees.indexOf(extractedDefaultTree);
+        trees.unshift(trees.splice(index, 1)[0]);
       }
 
       widgets.push({
