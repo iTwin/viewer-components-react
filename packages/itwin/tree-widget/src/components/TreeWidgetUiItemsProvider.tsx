45533233800b86ab581d4947166228a0aae37ba4
Jay Patel <pateljaypj1397@gmail.com>
2022-09-12 13:52:30 -0400

    Updated "extractedDefaultTreeId" to extractedDefaultTree

--- packages/itwin/tree-widget/src/components/TreeWidgetUiItemsProvider.tsx
+++ packages/itwin/tree-widget/src/components/TreeWidgetUiItemsProvider.tsx
@@ -101,8 +101,8 @@ export class TreeWidgetUiItemsProvider implements UiItemsProvider {
       if(this._treeWidgetOptions?.defaultTree && trees.length !== 0){
         //Adding the defaultTree to first index
         const defaultTreeId = this._treeWidgetOptions.defaultTree;
-        const extractedDefaultTreeId = trees.filter(tree => tree.id===defaultTreeId)[0];
-        const index = trees.indexOf(extractedDefaultTreeId);
+        const extractedDefaultTree = trees.filter(tree => tree.id===defaultTreeId)[0];
+        const index = trees.indexOf(extractedDefaultTree);
         trees.unshift(trees.splice(index, 1)[0]);
       }
 
