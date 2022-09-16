6f51c6fdb00204fa232b1c9017adc17831f41eb9
Arun George <11051042+aruniverse@users.noreply.github.com>
2022-09-16 15:54:38 -0400

    Update packages/itwin/grouping-mapping-widget/src/widget/components/GroupingMapping.tsx

--- packages/itwin/grouping-mapping-widget/src/widget/components/GroupingMapping.tsx
+++ packages/itwin/grouping-mapping-widget/src/widget/components/GroupingMapping.tsx
@@ -35,7 +35,7 @@ export interface GroupingMappingProps {
    */
   client?: IMappingsClient;
   /**
-   * Custom UI providers to add and update groups.
+   * Custom UI to add and update groups.
    */
   customUIs?: GroupingMappingCustomUI[];
 }
