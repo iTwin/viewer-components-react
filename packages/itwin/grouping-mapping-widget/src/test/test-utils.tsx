1ab1b2f905b7173ec5f9ca596706f6a961eefad7
Guillar1 <Guillar1@users.noreply.github.com>
2023-01-24 15:45:02 -0800

    Update test-utils.tsx

--- packages/itwin/grouping-mapping-widget/src/test/test-utils.tsx
+++ packages/itwin/grouping-mapping-widget/src/test/test-utils.tsx
@@ -4,14 +4,6 @@
 *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
 import { render as rtlRender } from "@testing-library/react";
-import type { Store } from "redux";
-import { combineReducers, createStore } from "redux";
-import {
-  FrameworkReducer,
-  SyncUiEventDispatcher,
-  UiFramework,
-} from "@itwin/appui-react";
-import type { IModelConnection } from "@itwin/core-frontend";
 import userEvent from "@testing-library/user-event";
 import type { IMappingsClient } from "@itwin/insights-client";
 import type { GroupingMappingCustomUI } from "../grouping-mapping-widget";
@@ -47,23 +39,3 @@ function render(ui: React.ReactElement, mockUIs: GroupingMappingCustomUI[] = [],
 export * from "@testing-library/react";
 // override React Testing Library's render with our own
 export { render };
-
-export class TestUtils {
-  public static createAppStore(): Store {
-    const rootReducer = combineReducers({
-      frameworkState: FrameworkReducer,
-    });
-
-    return createStore(
-      rootReducer,
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-    );
-  }
-
-  public static async initializeUiFramework(_?: IModelConnection) {
-    const store = this.createAppStore();
-    await UiFramework.initialize(store);
-    SyncUiEventDispatcher.setTimeoutPeriod(0); // disables non-immediate event processing.
-  }
-}
