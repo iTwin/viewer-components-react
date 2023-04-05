93d4688eff3f2e0fb9a459eac7ca0e4ff6cd923f
Jake-Screen <Jake-Screen@users.noreply.github.com>
2023-04-05 15:22:02 -0400

    comments

--- packages/itwin/grouping-mapping-widget/src/widget/components/CalculatedPropertyTable.tsx
+++ packages/itwin/grouping-mapping-widget/src/widget/components/CalculatedPropertyTable.tsx
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import { SvgDelete, SvgEdit, SvgMore } from "@itwin/itwinui-icons-react";
 import {
   DropdownMenu,
