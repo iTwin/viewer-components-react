e4af98ff4f0121579fe31c53f4a6a63793650f88
grigas.petraitis <35135765+grigasp@users.noreply.github.com>
2023-04-05 11:18:00 +0300

    Add external sources tree component (`@alpha`)

--- packages/itwin/tree-widget/src/components/trees/index.ts
+++ packages/itwin/tree-widget/src/components/trees/index.ts
@@ -10,6 +10,8 @@ export * from "./VisibilityTreeRenderer";
 export * from "./category-tree/CategoriesTree";
 export * from "./category-tree/CategoriesTreeComponent";
 export * from "./category-tree/CategoryVisibilityHandler";
+export * from "./external-sources-tree/ExternalSourcesTree";
+export * from "./external-sources-tree/ExternalSourcesTreeComponent";
 export * from "./models-tree/ModelsTree";
 export * from "./models-tree/ModelsTreeComponent";
 export * from "./models-tree/ModelsVisibilityHandler";
