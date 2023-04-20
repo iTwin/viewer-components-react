1b2ae386e5d9a3375f41f7843a2314b72cd6f630
Jake-Screen <Jake-Screen@users.noreply.github.com>
2023-04-20 16:53:04 -0400

    lint

--- packages/itwin/breakdown-trees/src/test/Utils.tsx
+++ packages/itwin/breakdown-trees/src/test/Utils.tsx
@@ -31,7 +31,7 @@ function createAppStore(): Store {
 
   return configureStore({
     reducer: rootReducer,
-    enhancers: (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+    enhancers: (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
   });
 }
 
