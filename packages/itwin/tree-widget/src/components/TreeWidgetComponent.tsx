ab252c48b47574a88be36bcaac6173e0fa6df7a4
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2023-04-25 10:12:58 +0300

    Tree Widget: Add ability to conditionally show/hide trees in widget (#444)
    * Add ability to conditionally show trees in the widget
    
    * rush change
    
    * PR fixes

--- packages/itwin/tree-widget/src/components/TreeWidgetComponent.tsx
+++ packages/itwin/tree-widget/src/components/TreeWidgetComponent.tsx
@@ -5,12 +5,25 @@
 
 import "./TreeWidgetComponent.scss";
 import * as React from "react";
-import { SelectableContent, SelectableContentDefinition } from "@itwin/components-react";
+import { useActiveIModelConnection } from "@itwin/appui-react";
+import { SelectableContent, SelectableContentDefinition, SelectableContentProps } from "@itwin/components-react";
+import { IModelConnection } from "@itwin/core-frontend";
+import { FillCentered } from "@itwin/core-react";
+import { ProgressLinear } from "@itwin/itwinui-react";
+import { TreeWidget } from "../TreeWidget";
 
 export interface TreeDefinition {
+  /** Id of the tree */
   id: string;
+  /** Callback that is used to get tree label */
   getLabel: () => string;
+  /** Callback that is used to render tree component */
   render: () => React.ReactNode;
+  /**
+   * Callback that is used to determine if tree should be shown for current active iModel connection.
+   * If callback is `undefined` tree is shown for all iModel connections.
+   */
+  shouldShow?: (imodel: IModelConnection) => Promise<boolean>;
 }
 
 interface TreeWidgetComponentProps {
@@ -18,18 +31,101 @@ interface TreeWidgetComponentProps {
 }
 
 export function TreeWidgetComponent(props: TreeWidgetComponentProps) {
-  const trees: SelectableContentDefinition[] = props.trees.map((tree) => ({
-    id: tree.id,
-    label: tree.getLabel(),
-    render: tree.render,
-  }));
+  const { trees: treeDefinitions } = props;
+  const trees = useActiveTrees(treeDefinitions);
 
   return (
     <div className="tree-widget-visibility-widget">
-      <SelectableContent
-        children={trees} // eslint-disable-line react/no-children-prop
-        defaultSelectedContentId={trees[0].id}
-      />
+      <SelectableContent {...getSelectableContentProps(trees)} />
     </div>
   );
+}
+
+function useActiveTrees(treeDefinitions: TreeDefinition[]) {
+  const [trees, setTrees] = React.useState<SelectableContentDefinition[]>();
+  const imodel = useActiveIModelConnection();
+
+  React.useEffect(() => {
+    if (!imodel)
+      return;
+
+    let disposed = false;
+    (async () => {
+      const visibleTrees = await getActiveTrees(treeDefinitions, imodel);
+      if (!disposed) {
+        setTrees(visibleTrees);
+      }
+    })();
+
+    return () => { disposed = true; };
+  }, [treeDefinitions, imodel]);
+
+  return trees;
+}
+
+async function getActiveTrees(treeDefinitions: TreeDefinition[], imodel: IModelConnection): Promise<SelectableContentDefinition[]> {
+  const handleDefinition = async (treeDef: TreeDefinition) => {
+    if (treeDef.shouldShow !== undefined && !(await treeDef.shouldShow(imodel))) {
+      return undefined;
+    }
+    return {
+      id: treeDef.id,
+      label: treeDef.getLabel(),
+      render: treeDef.render,
+    };
+  };
+
+  return (await Promise.all(treeDefinitions.map(handleDefinition))).filter((tree) => tree !== undefined) as SelectableContentDefinition[];
+}
+
+function getSelectableContentProps(trees?: SelectableContentDefinition[]): SelectableContentProps {
+  if (trees === undefined) {
+    return {
+      defaultSelectedContentId: "loading",
+      children: [{
+        id: "loading",
+        label: "",
+        render: () => (
+          <Delayed>
+            <ProgressLinear indeterminate={true} />
+          </Delayed>
+        ),
+      }],
+    };
+  }
+
+  if (trees.length === 0) {
+    return {
+      defaultSelectedContentId: "no-trees",
+      children: [{
+        id: "no-trees",
+        label: "",
+        render: () => (
+          <FillCentered>
+            {TreeWidget.translate("noTrees")}
+          </FillCentered>
+        ),
+      }],
+    };
+  }
+
+  return {
+    defaultSelectedContentId: trees[0].id,
+    children: trees,
+  };
+}
+
+function Delayed({ delay = 200, children }: React.PropsWithChildren<{ delay?: number }>) {
+  const [show, setShow] = React.useState(false);
+
+  React.useEffect(() => {
+    const id = setTimeout(() => { setShow(true); }, delay);
+    return () => { clearTimeout(id); };
+  }, [delay]);
+
+  if (!show) {
+    return null;
+  }
+
+  return <>{children}</>;
 }
