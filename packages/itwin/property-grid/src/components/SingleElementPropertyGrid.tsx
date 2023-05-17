480e0aa6d28c193fc6003b6899b2c134a7435c6c
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2023-05-17 09:21:41 +0300

    Property Widget: Cleanup data provider config (#484)
    * Cleanup data provider configuration
    
    * rush change

--- packages/itwin/property-grid/src/components/SingleElementPropertyGrid.tsx
+++ packages/itwin/property-grid/src/components/SingleElementPropertyGrid.tsx
@@ -5,64 +5,26 @@
 
 import { useEffect } from "react";
 import { KeySet } from "@itwin/presentation-common";
-import { useCustomDataProvider, useDefaultDataProvider } from "../hooks/UseDataProvider";
+import { useDataProvider } from "../hooks/UseDataProvider";
 import { PropertyGridContent } from "./PropertyGridContent";
 
 import type { IModelConnection } from "@itwin/core-frontend";
 import type { InstanceKey } from "@itwin/presentation-common";
 import type { PropertyGridContentProps } from "./PropertyGridContent";
-import type { CustomDataProviderProps, DefaultDataProviderProps } from "../hooks/UseDataProvider";
+import type {  DataProviderProps } from "../hooks/UseDataProvider";
 
-/** Base props for data provider used by `SingleElementPropertyGrid. */
-export interface SingleElementDataProviderBaseProps {
+/** Props for data provider used by `SingleElementPropertyGrid. */
+export interface SingleElementDataProviderProps extends DataProviderProps {
   /** Key of the instance which data should be loaded. */
   instanceKey: InstanceKey;
 }
-
-/** Props for configuring default data provider used by `SingleElementPropertyGrid`. */
-export type SingleElementDefaultDataProviderProps = DefaultDataProviderProps & SingleElementDataProviderBaseProps;
-
-/** Props for providing custom data provider that will be used by `SingleElementPropertyGrid`. */
-export type SingleElementCustomDataProviderProps = CustomDataProviderProps & SingleElementDataProviderBaseProps;
-
-/** Props for data provider used by `SingleElementPropertyGrid`. */
-export type SingleElementDataProviderProps = SingleElementDefaultDataProviderProps | SingleElementCustomDataProviderProps;
 
 /** Props for `SingleElementPropertyGrid` component. */
 export type SingleElementPropertyGridProps = Omit<PropertyGridContentProps, "dataProvider"> & SingleElementDataProviderProps;
 
 /** Component that renders property grid for single element. */
-export function SingleElementPropertyGrid(props: SingleElementPropertyGridProps) {
-  if (isCustomDataProviderProps(props)) {
-    return <SingleElementCustomPropertyGrid {...props} />;
-  }
-
-  return <SingleElementDefaultPropertyGrid {...props} />;
-}
-
-function SingleElementDefaultPropertyGrid({ instanceKey, enableFavoriteProperties, enablePropertyGroupNesting, rulesetId,...props }: SingleElementPropertyGridProps & SingleElementDefaultDataProviderProps) {
-  const dataProvider = useSingleElementDataProvider({
-    imodel: props.imodel,
-    instanceKey,
-    enableFavoriteProperties,
-    enablePropertyGroupNesting,
-    rulesetId,
-  });
-
-  return (
-    <PropertyGridContent
-      {...props}
-      dataProvider={dataProvider}
-    />
-  );
-}
-
-function SingleElementCustomPropertyGrid({ instanceKey, createDataProvider,...props }: SingleElementPropertyGridProps & SingleElementCustomDataProviderProps) {
-  const dataProvider = useSingleElementCustomDataProvider({
-    imodel: props.imodel,
-    instanceKey,
-    createDataProvider,
-  });
+export function SingleElementPropertyGrid({ instanceKey, createDataProvider, ...props }: SingleElementPropertyGridProps) {
+  const dataProvider = useSingleElementDataProvider({ imodel: props.imodel, instanceKey, createDataProvider  });
 
   return (
     <PropertyGridContent
@@ -73,23 +35,10 @@ function SingleElementCustomPropertyGrid({ instanceKey, createDataProvider,...pr
 }
 
 /** Custom hook that creates data provider and setup it to load data for specific instance. */
-function useSingleElementDataProvider({ instanceKey, ...props }: SingleElementDefaultDataProviderProps & { imodel: IModelConnection }) {
-  const dataProvider = useDefaultDataProvider(props);
+function useSingleElementDataProvider({ instanceKey, ...props }: SingleElementDataProviderProps & { imodel: IModelConnection }) {
+  const dataProvider = useDataProvider(props);
   useEffect(() => {
     dataProvider.keys = new KeySet([instanceKey]);
   }, [dataProvider, instanceKey]);
   return dataProvider;
-}
-
-/** Custom hook that creates custom data provider and setup it to load data for specific instance. */
-function useSingleElementCustomDataProvider({ instanceKey, ...props }: SingleElementCustomDataProviderProps & { imodel: IModelConnection }) {
-  const dataProvider = useCustomDataProvider(props);
-  useEffect(() => {
-    dataProvider.keys = new KeySet([instanceKey]);
-  }, [dataProvider, instanceKey]);
-  return dataProvider;
-}
-
-function isCustomDataProviderProps(props: SingleElementPropertyGridProps): props is SingleElementPropertyGridProps & SingleElementCustomDataProviderProps {
-  return (props as SingleElementCustomDataProviderProps).createDataProvider !== undefined;
 }
