aa9f79b55dc797570129660b308d5fc2a8b8d13c
Wil Maier <48260215+wilmaier@users.noreply.github.com>
2023-05-24 10:23:02 -0400

    #1195090 Content in measurements widget needs some padding

--- packages/itwin/measure-tools/src/ui-2.0/MeasurementPropertyWidget.tsx
+++ packages/itwin/measure-tools/src/ui-2.0/MeasurementPropertyWidget.tsx
@@ -2,6 +2,7 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
+import "./MeasurementPropertyWidget.scss";
 import * as React from "react";
 import type { Id64String } from "@itwin/core-bentley";
 import { IModelApp, SelectionSetEventType } from "@itwin/core-frontend";
