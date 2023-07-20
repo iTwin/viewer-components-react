b8e5e76460016e7661b5014037dc450260284295
Do Thien An Duong <116378951+AnDuong249@users.noreply.github.com>
2023-07-20 11:43:09 -0700

    Resolve lint errors

--- packages/itwin/measure-tools/src/api/MeasurementManager.ts
+++ packages/itwin/measure-tools/src/api/MeasurementManager.ts
@@ -454,9 +454,9 @@ export class MeasurementManager implements Decorator {
     if (undefined === this._dropQuantityFormatterListeners) {
       const unsubscribers = [IModelApp.quantityFormatter.onActiveFormattingUnitSystemChanged.addListener(this.onActiveUnitSystemChanged, this),
         IModelApp.quantityFormatter.onQuantityFormatsChanged.addListener(this.onActiveUnitSystemChanged, this),
-        IModelApp.quantityFormatter.onUnitsProviderChanged.addListener(this.onActiveUnitSystemChanged, this)]
+        IModelApp.quantityFormatter.onUnitsProviderChanged.addListener(this.onActiveUnitSystemChanged, this)];
       this._dropQuantityFormatterListeners = () => unsubscribers.forEach((unsubscriber) => {
-        unsubscriber()
+        unsubscriber();
       });
 
     } else {
