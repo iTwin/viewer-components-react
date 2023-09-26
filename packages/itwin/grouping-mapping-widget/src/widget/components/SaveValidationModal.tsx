dd3be26df2245701535da7fc7991c086cf66d810
Guillar1 <Guillar1@users.noreply.github.com>
2023-09-26 11:02:35 -0700

    merge

---
+++ packages/itwin/grouping-mapping-widget/src/widget/components/SaveValidationModal.tsx
@@ -0,0 +1,95 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+import {
+  Button,
+  Modal,
+  ModalButtonBar,
+  Table,
+  Text,
+} from "@itwin/itwinui-react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import type { CreateTypeFromInterface } from "../utils";
+import type { InvalidCalculations } from "./PropertyValidationUtils";
+
+export interface SaveValidationModalProps {
+  onSave: (rows: string) => void;
+  onClose: () => void;
+  invalidCustomCalcs: InvalidCalculations[];
+  showSaveValidationModal: boolean;
+  setInvalidCustomCalcs: (updatesCals: InvalidCalculations[]) => void;
+}
+
+export const SaveValidationModal = ({
+  onSave,
+  onClose,
+  showSaveValidationModal,
+  invalidCustomCalcs,
+}: SaveValidationModalProps) => {
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const selectedRows = useRef("");
+
+  useEffect(() => {
+    if (invalidCustomCalcs.length > 0) {
+      setIsLoading(false);
+    }
+  }, [invalidCustomCalcs]);
+
+  const onSelect = useCallback(async (rows) => {
+    selectedRows.current = JSON.stringify(rows);
+  }, []);
+
+  const handleOnSave = useCallback(() => {
+    onSave(selectedRows.current);
+  }, [onSave]);
+
+  const columns = useMemo(() => [{
+    id: "customCalcName",
+    Header: "Custom Calculation",
+    accessor: "customCalcName" as keyof InvalidCalculations,
+  }, {
+    id: "origFormula",
+    Header: "Original Formula",
+    accessor: "origFormula" as keyof InvalidCalculations,
+  }, {
+    id: "changedFormula",
+    Header: "Formula",
+    accessor: "changedFormula" as keyof InvalidCalculations,
+  }], []);
+
+  return (
+    <Modal
+      title='Confirm Update'
+      modalRootId='grouping-mapping-widget'
+      isOpen={showSaveValidationModal}
+      onClose={onClose}
+    >
+      <Text variant="leading" as="h3">
+        Update to this property will cause the following custom calculations to be invalid. Do you also want to make the suggested changes to their formula(s) ?
+        Please select the custom calculation(s) that you want to update along with the change in the property.
+      </Text>
+      <Table<CreateTypeFromInterface<InvalidCalculations>>
+        columns={columns}
+        data={isLoading ? [] : invalidCustomCalcs}
+        emptyTableContent='No data.'
+        isSelectable={true}
+        isLoading={isLoading}
+        onSelect={onSelect}
+        selectionMode='multi'
+      />
+      <ModalButtonBar>
+        <Button styleType='high-visibility' onClick={handleOnSave}>
+          Save
+        </Button>
+        <Button
+          onClick={onClose}
+          styleType='default'
+        >
+          Cancel
+        </Button>
+      </ModalButtonBar>
+    </Modal>
+  );
+};
