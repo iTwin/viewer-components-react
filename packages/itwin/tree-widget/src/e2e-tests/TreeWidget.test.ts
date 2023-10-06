01abc2da43943f43b5eb1afa6575c3b36d6edb22
Michel D'Astous <mdastous-bentley@users.noreply.github.com>
2023-10-06 08:49:59 -0400

    Enable notification messages in test-viewer (#667)

--- packages/itwin/tree-widget/src/e2e-tests/TreeWidget.test.ts
+++ packages/itwin/tree-widget/src/e2e-tests/TreeWidget.test.ts
@@ -2,10 +2,11 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
+
 import type { Locator } from "@playwright/test";
-import { expect, test } from "@playwright/test";
 import assert from "assert";
-import { expandStagePanel, locateNode, locateWidget } from "./utils";
+import { expect, test } from "@playwright/test";
+import { expandStagePanel, locateNode, locateWidget, takeScreenshot } from "./utils";
 
 let treeWidget: Locator;
 test.beforeEach(async ({ page, baseURL }) => {
@@ -19,36 +20,36 @@ test.beforeEach(async ({ page, baseURL }) => {
 
 test.describe("should match image snapshot", () => {
 
-  test("initial tree", async () => {
+  test("initial tree", async ({ page }) => {
     // wait for element to be visible in the tree
     await locateNode(treeWidget, "ProcessPhysicalModel").getByRole("checkbox", { name: "Visible", exact: true }).waitFor();
-    await expect(treeWidget).toHaveScreenshot();
+    await takeScreenshot(page, treeWidget);
   });
 
-  test("expanded tree node", async () => {
+  test("expanded tree node", async ({ page }) => {
     const node = locateNode(treeWidget, "ProcessPhysicalModel");
     await node.getByTestId("tree-node-expansion-toggle").click();
 
     // wait for node at the bottom to be visible/loaded
     await locateNode(treeWidget, "Tag-Category").waitFor();
-    await expect(treeWidget).toHaveScreenshot();
+    await takeScreenshot(page, treeWidget);
   });
 
-  test("selected node", async () => {
+  test("selected node", async ({ page }) => {
     const node = locateNode(treeWidget, "BayTown");
     await node.click();
 
     // wait for node to become selected
     await expect(node).toHaveClass(/is-selected/);
-    await expect(treeWidget).toHaveScreenshot();
+    await takeScreenshot(page, treeWidget);
   });
 
-  test("search", async () => {
+  test("search", async ({ page }) => {
     await treeWidget.getByText("BayTown").waitFor();
     await treeWidget.getByTitle("Search for something").click();
     await treeWidget.getByPlaceholder("Search...").fill("Model");
     await treeWidget.locator(".components-activehighlight").waitFor();
-    await expect(treeWidget).toHaveScreenshot();
+    await takeScreenshot(page, treeWidget);
   });
 
 });
