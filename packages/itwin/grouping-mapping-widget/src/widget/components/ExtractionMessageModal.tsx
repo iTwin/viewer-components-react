8b24999d423ee6c032a3de9189b2fddc96fbaa20
Uyen Doan <smmr-dn@users.noreply.github.com>
2023-10-09 16:49:26 -0500

    Removed iModelId and replaced MappingId and GroupId with names in log messages.

--- packages/itwin/grouping-mapping-widget/src/widget/components/ExtractionMessageModal.tsx
+++ packages/itwin/grouping-mapping-widget/src/widget/components/ExtractionMessageModal.tsx
@@ -3,9 +3,12 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 import { Button, DefaultCell, Icon, Modal, ModalButtonBar, ModalContent, Table, tableFilters, Text } from "@itwin/itwinui-react";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import type { CreateTypeFromInterface } from "../utils";
-import type { ExtractionMessageData } from "./hooks/useMappingsOperations";
+import type { ExtractionMessageData} from "./hooks/useMappingsOperations";
+import { useMappingsOperations } from "./hooks/useMappingsOperations";
+import { useGroupingMappingApiConfig } from "./context/GroupingApiConfigContext";
+import { useMappingClient } from "./context/MappingClientContext";
 import "./ExtractionMessageModal.scss";
 import { SvgClock } from "@itwin/itwinui-icons-react";
 import type { CellRendererProps, Column } from "react-table";
@@ -21,6 +24,42 @@ export interface ExtractionMessageModalProps {
 
 export const ExtractionMessageModal = ({ isOpen, onClose, extractionMessageData, timestamp }: ExtractionMessageModalProps) => {
   const [formattedTimestamp, setFormattedTimestamp] = useState<string>("");
+  const groupingMappingApiConfig = useGroupingMappingApiConfig();
+  const mappingClient = useMappingClient();
+  const [mappingId, setMappingId] = useState<string>("");
+  const [groupId, setGroupId]  = useState<string>("");
+  const { mappings } = useMappingsOperations({...groupingMappingApiConfig, mappingClient});
+  useEffect(() => {
+    extractionMessageData.forEach((extractionMessage) => {
+      if(extractionMessage.message.includes("iModel")){
+        extractionMessage.message = extractionMessage.message.replace(/iModel [\w-]+/, "iModel");
+      }
+    });
+  }, [extractionMessageData]);
+  const setMappingandGroupNames = useCallback (async (extractionMessage: ExtractionMessageData) => {
+    if(mappingId){
+      const mappingName = mappings.filter((mapping) => {return mapping.id === mappingId;});
+      const accessToken = await groupingMappingApiConfig.getAccessToken();
+      const groups = await mappingClient.getGroups(
+        accessToken,
+        groupingMappingApiConfig.iModelId,
+        mappingId
+      );
+      const groupName = groups.filter((group) => {return group.id === groupId;});
+      extractionMessage.message = extractionMessage.message.replace(/MappingId: [\w-]+/, `Mapping: ${  mappingName[0].mappingName}`);
+      extractionMessage.message = extractionMessage.message.replace(/GroupingId: [\w-]+/, `Group: ${  groupName[0].groupName}`);
+    }
+  }, [mappingId, groupId, groupingMappingApiConfig, mappings, mappingClient]);
+  useEffect(() => {
+    extractionMessageData.forEach(async (extractionMessage) => {
+      if(extractionMessage.message.includes("MappingId:")){
+        const splittedMessage = extractionMessage.message.split(" ");
+        setMappingId(splittedMessage[splittedMessage.indexOf("MappingId:") + 1]);
+        setGroupId(splittedMessage[splittedMessage.indexOf("GroupingId:") + 1]);
+        await setMappingandGroupNames(extractionMessage);
+      }
+    });
+  }, [extractionMessageData, setMappingandGroupNames]);
   useEffect(() => {
     const newDateTime: Date = new Date(timestamp);
     const options: Intl.DateTimeFormatOptions = {
@@ -29,6 +68,7 @@ export const ExtractionMessageModal = ({ isOpen, onClose, extractionMessageData,
     };
     setFormattedTimestamp(newDateTime.toLocaleString(undefined, options));
   }, [timestamp]);
+
   const translatedLabels = useMemo(() => ({
     filter: "Filter",
     clear: "Clear",
