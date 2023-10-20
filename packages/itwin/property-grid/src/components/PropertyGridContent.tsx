c750dcf0bb203d10d522713d5030b5d7c0385a17
Vilius Bakutis <114080994+ViliusBa@users.noreply.github.com>
2023-10-20 17:57:46 +0300

    initial implementation

--- packages/itwin/property-grid/src/components/PropertyGridContent.tsx
+++ packages/itwin/property-grid/src/components/PropertyGridContent.tsx
@@ -5,14 +5,14 @@
 
 import "./PropertyGridContent.scss";
 import classnames from "classnames";
-import { useCallback, useState } from "react";
-import { PropertyValueRendererManager } from "@itwin/components-react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { CompositeFilterType, CompositePropertyDataFilterer, DisplayValuePropertyDataFilterer, FilteredType, LabelPropertyDataFilterer, PropertyCategoryLabelFilterer, PropertyValueRendererManager } from "@itwin/components-react";
 import { ResizableContainerObserver } from "@itwin/core-react";
-import { Text } from "@itwin/itwinui-react";
+import { SearchBox, Text } from "@itwin/itwinui-react";
 import { useContextMenu } from "../hooks/UseContextMenu";
 import { useLoadedInstanceInfo } from "../hooks/UseInstanceInfo";
 import { useNullValueSettingContext } from "../hooks/UseNullValuesSetting";
-import { FilteringPropertyGrid, NonEmptyValuesPropertyDataFilterer, NoopPropertyDataFilterer } from "./FilteringPropertyGrid";
+import { FilteringPropertyGrid, NonEmptyValuesPropertyDataFilterer } from "./FilteringPropertyGrid";
 import { Header } from "./Header";
 import { SettingsDropdownMenu } from "./SettingsDropdownMenu";
 
@@ -20,7 +20,7 @@ import type { SettingsDropdownMenuProps, SettingsMenuProps } from "./SettingsDro
 import type { ReactNode } from "react";
 import type { PropertyRecord } from "@itwin/appui-abstract";
 import type { IModelConnection } from "@itwin/core-frontend";
-import type { PropertyCategory, PropertyUpdatedArgs } from "@itwin/components-react";
+import type { PropertyCategory , PropertyUpdatedArgs } from "@itwin/components-react";
 import type { IPresentationPropertyDataProvider } from "@itwin/presentation-components";
 import type { FilteringPropertyGridProps } from "./FilteringPropertyGrid";
 import type { ContextMenuProps } from "../hooks/UseContextMenu";
@@ -78,8 +78,10 @@ export function PropertyGridContent({
     imodel,
     contextMenuItems,
   });
+
+  const [filterText, setFilterText] = useState<string>("");
   const { showNullValues } = useNullValueSettingContext();
-  const filterer = useFilterer({ showNullValues });
+  const filterer = useFilterer({ showNullValues, filterText });
 
   const [{ width, height }, setSize] = useState({ width: 0, height: 0 });
   const handleResize = useCallback((w: number, h: number) => {
@@ -95,6 +97,7 @@ export function PropertyGridContent({
     ...props,
     dataProvider,
     filterer,
+    highlight: filterText && filterText.length !== 0 ? { highlightedText: filterText, filteredTypes: [FilteredType.Category, FilteredType.Label, FilteredType.Value] } : undefined,
     isPropertyHoverEnabled: true,
     isPropertySelectionEnabled: true,
     onPropertyContextMenu,
@@ -105,10 +108,21 @@ export function PropertyGridContent({
       : undefined,
   };
 
+  const onFilterStart = (searchText: string) => {
+    setFilterText(searchText);
+  };
+
+  const onFilterClear = () => {
+    setFilterText("");
+  };
+
   return (
     <div className={classnames("property-grid-react-container", className)}>
-      <PropertyGridHeader controls={headerControls} item={item} onBackButtonClick={onBackButton} settingsProps={settingsProps} />
+      <div>
+        <PropertyGridHeader controls={headerControls} item={item} onBackButtonClick={onBackButton} settingsProps={settingsProps} onFilterStart={onFilterStart} onFilterClear={onFilterClear} />
+      </div>
       <div className="property-grid-react-data">
+
         <ResizableContainerObserver onResize={handleResize}>
           {
             dataRenderer
@@ -127,40 +141,174 @@ interface PropertyGridHeaderProps {
   item?: { className: string, label: PropertyRecord };
   onBackButtonClick?: () => void;
   settingsProps: SettingsDropdownMenuProps;
+  onFilterStart: (searchText: string) => void;
+  onFilterClear: () => void;
 }
 
-function PropertyGridHeader({ item, controls, settingsProps, onBackButtonClick }: PropertyGridHeaderProps) {
+function PropertyGridHeader({ item, controls, settingsProps, onBackButtonClick, onFilterStart, onFilterClear }: PropertyGridHeaderProps) {
+  const [filteringSearchBar, setFilteringSearchBar] = useState(false);
+  const [inputValue, setInputValue] = useState<string>("");
+  const onFilterStartRef = useRef(onFilterStart);
+  // save latest `onFiltertStart` reference into `useRef` to avoid restarting timeout when `onFiltertStart` reference changes.
+  onFilterStartRef.current = onFilterStart;
+
+  useEffect(() => {
+    if (!inputValue) {
+      onFilterStartRef.current("");
+      return;
+    }
+
+    const timeoutId = setTimeout(() => {
+      onFilterStartRef.current(inputValue);
+    }, 50);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [inputValue]);
+
   if (!item) {
     return null;
   }
 
+  // <FilteringInput
+  //       onFilterCancel={() => {
+  //         onFilterClear();
+  //         setFilteringSearchBar(false);
+  //       }}
+  //       onFilterClear={() => {
+  //         setFilteringStatus(0);
+  //       }}
+  //       onFilterStart={(searchText: string) => {
+  //         onFilterStart(searchText);
+  //         setFilteringStatus(1);
+  //       }}
+  //       // style={{
+
+  //       //  }}
+  //       status={filteringStatus}
+  //     />
+
+  // return <SearchBox
+  //   expandable
+  //   onExpand={onOpen}
+  //   onCollapse={onClose}
+  //   className={classnames("tree-widget-search-box", !isOpened && "contracted")}
+  // >
+  //   <SearchBox.CollapsedState>
+  //     <SearchBox.ExpandButton
+  //       title={TreeWidget.translate("searchBox.searchForSomething")}
+  //       aria-label={TreeWidget.translate("searchBox.open")}
+  //       size="small"
+  //     />
+  //   </SearchBox.CollapsedState>
+  //   <SearchBox.ExpandedState >
+  //     <SearchBox.Input
+  //       placeholder={TreeWidget.translate("searchBox.search")}
+  //       onChange={(e) => setInputValue(e.currentTarget.value)}
+  //       className="search-input"
+  //     />
+  //     <SearchResultStepper
+  //       selectedIndex={selectedResultIndex}
+  //       total={resultCount}
+  //       onStep={onSelectedResultChanged}
+  //     />
+  //     <SearchBox.CollapseButton
+  //       onClick={() => {
+  //         setInputValue("");
+  //         onClose();
+  //       }}
+  //       size="small"
+  //       aria-label={TreeWidget.translate("searchBox.close")}
+  //     />
+  //   </SearchBox.ExpandedState>
+  // </SearchBox>;
+
   return (
+  // <div><div> {!filteringSearchBar ? undefined :
+  //   <SearchBox
+  //     onCollapse={() => setFilteringSearchBar(false)}
+  //   >
+  //     <SearchBox.ExpandedState >
+  //       <SearchBox.Input
+  //         onChange={(e) => setInputValue(e.currentTarget.value)}
+  //       />
+  //       <SearchBox.CollapseButton
+  //         onClick={() => {
+  //           onFilterClear();
+  //         }}
+  //       />
+  //     </SearchBox.ExpandedState>
+  //   </SearchBox>
+  // }</div>
+
     <Header onBackButtonClick={onBackButtonClick}>
       <div className="property-grid-react-panel-label-and-class">
-        <Text variant="leading">
+        {!filteringSearchBar ? <div><Text variant="leading">
           {PropertyValueRendererManager.defaultManager.render(item.label)}
         </Text>
-        <Text>{item.className}</Text>
-      </div>
+        <Text>{item.className}</Text></div> : undefined
+
+        }</div><SearchBox
+        expandable
+        onCollapse={() => setFilteringSearchBar(false)}
+        onExpand={() => setFilteringSearchBar(true)}
+        className={classnames("ooga-booga", !filteringSearchBar && "contracted")}
+      >
+        <SearchBox.CollapsedState>
+          <SearchBox.ExpandButton
+            size="small"
+          />
+        </SearchBox.CollapsedState>
+        <SearchBox.ExpandedState >
+          <SearchBox.Input
+            placeholder={"Search"}
+            onChange={(e) => setInputValue(e.currentTarget.value)}
+          />
+          <SearchBox.CollapseButton
+            onClick={() => {
+              onFilterClear();
+            }}
+          />
+        </SearchBox.ExpandedState>
+      </SearchBox>
+      {/* <Text variant="leading">
+          {PropertyValueRendererManager.defaultManager.render(item.label)}
+        </Text>
+        <Text>{item.className}</Text> */}
+
       {controls}
+
+      {/* {filteringSearchBar ? undefined : <SvgSearch onClick={() => { setFilteringSearchBar(true);}}/>} */}
       <SettingsDropdownMenu {...settingsProps} />
     </Header>
+    // </div>
   );
 }
 
 interface UseFiltererProps {
   showNullValues: boolean;
+  filterText: string;
 }
 
-function useFilterer({ showNullValues }: UseFiltererProps) {
+function useFilterer({ showNullValues, filterText }: UseFiltererProps) {
   const [defaultFilterers] = useState(() => ({
-    noop: new NoopPropertyDataFilterer(),
     nonEmpty: new NonEmptyValuesPropertyDataFilterer(),
   }));
 
-  if (!showNullValues) {
-    return defaultFilterers.nonEmpty;
-  }
+  const compositeFilterer = useMemo(() => {
+    const valueFilterer = new DisplayValuePropertyDataFilterer(filterText);
+    const labelFilterer = new LabelPropertyDataFilterer(filterText);
+    const categoryFilterer = new PropertyCategoryLabelFilterer(filterText);
+    const valueAndRecordFilterer = new CompositePropertyDataFilterer(valueFilterer, CompositeFilterType.Or, labelFilterer);
+    const textFilterer = new CompositePropertyDataFilterer(valueAndRecordFilterer, CompositeFilterType.Or, categoryFilterer);
 
-  return defaultFilterers.noop;
+    if(!showNullValues){
+      return new CompositePropertyDataFilterer(textFilterer, CompositeFilterType.And, defaultFilterers.nonEmpty);
+    }
+
+    return textFilterer;
+  }, [defaultFilterers.nonEmpty, filterText, showNullValues]);
+
+  return compositeFilterer;
 }
