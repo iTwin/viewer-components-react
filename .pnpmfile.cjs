776e668c2efe09b72256efbf24db2e0ebc5aa863
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2023-12-13 17:46:20 +0200

    Fix missmatched peer dependency versions

--- .pnpmfile.cjs
+++ .pnpmfile.cjs
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-function readPackage(pkg) {
+function readPackage(pkg, context) {
   if (pkg.dependencies["@types/react"] === "*") {
     pkg.dependencies["@types/react"] = undefined;
     pkg.peerDependencies["@types/react"] = "*";
@@ -18,6 +18,25 @@ function readPackage(pkg) {
     pkg.peerDependencies["react"] = "*";
   }
 
+  // patch peer dependencies of packages from `itwinjs-core` to make sure that they are resolved to same version.
+  // peer dependencies in those packages are defined with `^` and that allows to resolve some of them to different version.
+  if (corePackages.includes(pkg.name) && pkg.peerDependencies) {
+    const fixedPeers = Object.entries(pkg.peerDependencies).reduce(
+      (deps, [name, version]) => ({ ...deps, [name]: corePackages.includes(name) ? version.replaceAll("^", "") : version }),
+      {},
+    );
+    pkg.peerDependencies = fixedPeers;
+  }
+
+  // patch peer dependencies of packages from `appui` to make sure that they are resolved to same version.
+  if (appuiPackages.includes(pkg.name) && pkg.peerDependencies) {
+    const fixedPeers = Object.entries(pkg.peerDependencies).reduce(
+      (deps, [name, version]) => ({ ...deps, [name]: appuiPackages.includes(name) ? version.replaceAll("^", "") : version }),
+      {},
+    );
+    pkg.peerDependencies = fixedPeers;
+  }
+
   return pkg;
 }
 
@@ -26,3 +45,30 @@ module.exports = {
     readPackage,
   },
 };
+
+const corePackages = [
+  "@itwin/appui-abstract",
+  "@itwin/core-bentley",
+  "@itwin/core-common",
+  "@itwin/core-frontend",
+  "@itwin/core-geometry",
+  "@itwin/core-i18n",
+  "@itwin/core-markup",
+  "@itwin/core-orbitgt",
+  "@itwin/core-quantity",
+  "@itwin/core-telemetry",
+  "@itwin/ecschema-metadata",
+  "@itwin/frontend-devtools",
+  "@itwin/map-layers-formats",
+  "@itwin/map-layers-auth",
+  "@itwin/presentation-common",
+  "@itwin/presentation-frontend",
+  "@itwin/webgl-compatibility",
+  "@itwin/appui-abstract",
+  "@itwin/build-tools",
+  "@itwin/certa",
+  "@itwin/core-backend",
+  "@itwin/presentation-backend",
+];
+
+const appuiPackages = ["@itwin/appui-layout-react", "@itwin/appui-react", "@itwin/components-react", "@itwin/core-react", "@itwin/imodel-components-react"];
