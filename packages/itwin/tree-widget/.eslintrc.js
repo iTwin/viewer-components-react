e5eebea087a3da64ee01365225991b5c67bc2260
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-01-17 09:54:51 +0200

    Cleanup

--- packages/itwin/tree-widget/.eslintrc.js
+++ packages/itwin/tree-widget/.eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
     "no-duplicate-imports": "off",
     "import/no-duplicates": "error",
     "object-curly-spacing": ["error", "always"],
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": ["error", { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" }],
     curly: ["error", "all"],
   },
 };
