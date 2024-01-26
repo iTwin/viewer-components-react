af09384bb8fed7663753434b3ec37b015eefce22
Michel D'Astous <mdastous-bentley@users.noreply.github.com>
2024-01-26 09:58:09 -0500

    wip

--- apps/test-viewer/src/components/Viewer.tsx
+++ apps/test-viewer/src/components/Viewer.tsx
@@ -4,10 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { FitViewTool, IModelApp, ScreenViewport, StandardViewId } from "@itwin/core-frontend";
+import { IModelApp, ScreenViewport } from "@itwin/core-frontend";
 import { FrontendDevTools } from "@itwin/frontend-devtools";
 import { ArcGisAccessClient } from "@itwin/map-layers-auth";
-import { Viewer as WebViewer, ViewerPerformance } from "@itwin/web-viewer-react";
+import { Viewer as WebViewer } from "@itwin/web-viewer-react";
 import { history } from "../history";
 import { getUiProvidersConfig } from "../UiProvidersConfig";
 import { ApiKeys } from "./ApiKeys";
@@ -66,29 +66,28 @@ function useViewCreatorOptions() {
    */
   const viewConfiguration = useCallback((viewPort: ScreenViewport) => {
     // default execute the fitview tool and use the iso standard view after tile trees are loaded
-    const tileTreesLoaded = () => {
-      return new Promise((resolve, reject) => {
-        const start = new Date();
-        const intvl = setInterval(() => {
-          if (viewPort.areAllTileTreesLoaded) {
-            ViewerPerformance.addMark("TilesLoaded");
-            ViewerPerformance.addMeasure("TileTreesLoaded", "ViewerStarting", "TilesLoaded");
-            clearInterval(intvl);
-            resolve(true);
-          }
-          const now = new Date();
-          // after 20 seconds, stop waiting and fit the view
-          if (now.getTime() - start.getTime() > 20000) {
-            reject();
-          }
-        }, 100);
-      });
-    };
-
-    tileTreesLoaded().finally(() => {
-      void IModelApp.tools.run(FitViewTool.toolId, viewPort, true, false);
-      viewPort.view.setStandardRotation(StandardViewId.Iso);
-    });
+    // const tileTreesLoaded = () => {
+    //   return new Promise((resolve, reject) => {
+    //     const start = new Date();
+    //     const intvl = setInterval(() => {
+    //       if (viewPort.areAllTileTreesLoaded) {
+    //         ViewerPerformance.addMark("TilesLoaded");
+    //         ViewerPerformance.addMeasure("TileTreesLoaded", "ViewerStarting", "TilesLoaded");
+    //         clearInterval(intvl);
+    //         resolve(true);
+    //       }
+    //       const now = new Date();
+    //       // after 20 seconds, stop waiting and fit the view
+    //       if (now.getTime() - start.getTime() > 20000) {
+    //         reject();
+    //       }
+    //     }, 100);
+    //   });
+    // };
+    // tileTreesLoaded().finally(() => {
+    //   void IModelApp.tools.run(FitViewTool.toolId, viewPort, true, false);
+    //   viewPort.view.setStandardRotation(StandardViewId.Iso);
+    // });
   }, []);
 
   return useMemo(() => ({ viewportConfigurer: viewConfiguration }), [viewConfiguration]);
--- packages/itwin/map-layers/src/ui/widget/FeatureInfoWidget.tsx
+++ packages/itwin/map-layers/src/ui/widget/FeatureInfoWidget.tsx
@@ -1,17 +1,15 @@
 /*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
-
+import { useActiveFrontstageDef, WidgetState } from "@itwin/appui-react";
 import { VirtualizedPropertyGridWithDataProvider } from "@itwin/components-react";
 import { FillCentered, Orientation, ResizableContainerObserver } from "@itwin/core-react";
-
-import { FeatureInfoDataProvider} from "./FeatureInfoDataProvider";
-import { MapFeatureInfoOptions } from "../Interfaces";
 import { MapLayersUI } from "../../mapLayers";
-import { useActiveFrontstageDef, WidgetState } from "@itwin/appui-react";
 import { FeatureInfoUiItemsProvider } from "../FeatureInfoUiItemsProvider";
+import { MapFeatureInfoOptions } from "../Interfaces";
+import { FeatureInfoDataProvider } from "./FeatureInfoDataProvider";
 
 export function useSpecificWidgetDef(id: string) {
   const frontstageDef = useActiveFrontstageDef();
@@ -33,9 +31,14 @@ export function MapFeatureInfoWidget({ featureInfoOpts }: MapFeatureInfoWidgetPr
 
   const [{ width, height }, setSize] = React.useState({ width: 0, height: 0 });
 
-  const handleDataChanged = () => {
-    setHasData(dataProvider.current !== undefined && dataProvider.current.hasRecords);
-  };
+  const widgetDef = useSpecificWidgetDef(FeatureInfoUiItemsProvider.widgetId);
+  const handleDataChanged = React.useCallback(() => {
+    const dataAvailable = dataProvider.current !== undefined && dataProvider.current.hasRecords;
+    setHasData(dataAvailable);
+    if (widgetDef) {
+      widgetDef.setWidgetState(dataAvailable ? WidgetState.Open : WidgetState.Hidden);
+    }
+  }, [widgetDef]);
 
   React.useEffect(() => {
     dataProvider.current = new FeatureInfoDataProvider();
@@ -44,44 +47,36 @@ export function MapFeatureInfoWidget({ featureInfoOpts }: MapFeatureInfoWidgetPr
     };
   }, []);
 
-  const widgetDef = useSpecificWidgetDef(FeatureInfoUiItemsProvider.widgetId);
-
   React.useEffect(() => {
-    if (hasData) {
-      widgetDef?.setWidgetState(WidgetState.Open);
-    } else {
-      widgetDef?.setWidgetState(WidgetState.Hidden);
-    }
-  }, [widgetDef, hasData]);
-
-  React.useEffect(() => {
-
     dataProvider.current?.onDataChanged.addListener(handleDataChanged);
     return () => {
       dataProvider.current?.onDataChanged.removeListener(handleDataChanged);
     };
-
-  }, []);
+  }, [handleDataChanged]);
 
   const handleResize = React.useCallback((w: number, h: number) => {
     setSize({ width: w, height: h });
   }, []);
 
-  if (!hasData) {
-    return (<FillCentered><span><i>{noRecordsMessage}</i></span></FillCentered>);
+  if (hasData && dataProvider.current) {
+    return (
+      <ResizableContainerObserver onResize={handleResize}>
+        <VirtualizedPropertyGridWithDataProvider
+          width={width}
+          height={height}
+          dataProvider={dataProvider.current}
+          orientation={Orientation.Vertical}
+          isPropertySelectionEnabled={featureInfoOpts?.propertyGridOptions?.isPropertySelectionEnabled}
+        />
+      </ResizableContainerObserver>
+    );
   } else {
-    if (dataProvider.current)
-      return (
-        <ResizableContainerObserver onResize={handleResize}>
-          <VirtualizedPropertyGridWithDataProvider
-            width={width}
-            height={height}
-            dataProvider={dataProvider.current}
-            orientation={Orientation.Vertical}
-            isPropertySelectionEnabled={featureInfoOpts?.propertyGridOptions?.isPropertySelectionEnabled} />
-        </ResizableContainerObserver>
-      );
-    else
-      return (<></>);
+    return (
+      <FillCentered>
+        <span>
+          <i>{noRecordsMessage}</i>
+        </span>
+      </FillCentered>
+    );
   }
 }
