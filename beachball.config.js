5659308e9096fd99a1ca6c52abfb97b7304fa2ae
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-01-30 11:54:28 +0200

    Change

--- beachball.config.js
+++ beachball.config.js
@@ -9,7 +9,7 @@ module.exports = {
   bumpDeps: false,
   access: "public",
   tag: "latest",
-  scope: ["packages/itwin/*", "!packages/itwin/tree-widget"],
+  scope: ["packages/itwin/*", "!packages/itwin/breakdown-trees"],
   ignorePatterns: [".nycrc", "eslint.config.js", ".mocharc.json", ".*ignore", ".github/**", ".vscode/**", "**/test/**", "pnpm-lock.yaml"],
   changehint: "Run 'pnpm change' to generate a change file",
   changelog: {
