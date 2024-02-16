5f71f4e1da9e8865e6257499db28ec1cbdf9c307
Guillar1 <Guillar1@users.noreply.github.com>
2024-02-16 12:12:35 -0800

    cr

--- packages/itwin/grouping-mapping-widget/src/components/Properties/hooks/useCalculatedPropertiesQuery.ts
+++ packages/itwin/grouping-mapping-widget/src/components/Properties/hooks/useCalculatedPropertiesQuery.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
 import type { IMappingsClient } from "@itwin/insights-client";
 import { useQuery } from "@tanstack/react-query";
 import type { GetAccessTokenFn } from "../../context/GroupingApiConfigContext";
