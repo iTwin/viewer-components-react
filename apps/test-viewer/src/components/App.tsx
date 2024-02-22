6fdb3812cde58b227261c856daf4b1704325ab80
Michel D'Astous <mdastous-bentley@users.noreply.github.com>
2024-02-22 08:35:29 -0500

    Removed resize observer errors work around

--- apps/test-viewer/src/components/App.tsx
+++ apps/test-viewer/src/components/App.tsx
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 import "./App.scss";
-import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { FillCentered } from "@itwin/core-react";
 import { ProgressLinear } from "@itwin/itwinui-react";
@@ -12,38 +11,7 @@ import { ArcGisOauthRedirect } from "./ArcGisOauthRedirect";
 import { AuthorizationProvider, AuthorizationState, SignInRedirect, useAuthorizationContext } from "./Authorization";
 import { Viewer } from "./Viewer";
 
-// Turn off errors related to resize Observers. We might want to
-// drop this we me migrate to react 18.
-// Based on this bug: https://github.com/iTwin/iTwinUI/issues/1317
-// and this post https://stackoverflow.com/questions/1146973/how-do-i-revert-all-local-changes-in-git-managed-project-to-previous-state
-export const useBeforeRender = (callback: any, deps: any) => {
-  const [isRun, setIsRun] = useState(false);
-  if (!isRun) {
-    callback();
-    setIsRun(true);
-  }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => () => setIsRun(false), deps);
-};
-
 export function App() {
-  useBeforeRender(() => {
-    window.addEventListener("error", (e) => {
-      if (e) {
-        const resizeObserverErrDiv = document.getElementById(
-          "webpack-dev-server-client-overlay-div",
-        );
-        const resizeObserverErr = document.getElementById(
-          "webpack-dev-server-client-overlay",
-        );
-        if (resizeObserverErr)
-          resizeObserverErr.className = "hide-resize-observer";
-        if (resizeObserverErrDiv)
-          resizeObserverErrDiv.className = "hide-resize-observer";
-      }
-    });
-  }, []);
-
   return (
     <AuthorizationProvider>
       <Routes>
