07a93fd812809c33cdb780d8267deb39f9b69c49
Dmitrij Kuzmiciov <yato333@users.noreply.github.com>
2024-04-12 17:45:45 +0300

    Refactor out visibility change functions

--- packages/itwin/tree-widget/src/components/trees/models-tree/ModelsVisibilityHandler.ts
+++ packages/itwin/tree-widget/src/components/trees/models-tree/ModelsVisibilityHandler.ts
@@ -3,11 +3,9 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { firstValueFrom, from, isObservable, lastValueFrom, mergeAll, mergeMap, toArray } from "rxjs";
+import { firstValueFrom, from, isObservable, mergeAll, mergeMap } from "rxjs";
 import { BeEvent } from "@itwin/core-bentley";
-import { IModelApp, PerModelCategoryVisibility } from "@itwin/core-frontend";
-import { NodeKey } from "@itwin/presentation-common";
-import { isPresentationTreeNodeItem } from "@itwin/presentation-components";
+import { IModelApp } from "@itwin/core-frontend";
 import { Presentation } from "@itwin/presentation-frontend";
 import { toggleAllCategories } from "../CategoriesVisibilityUtils";
 import { ElementIdsCache } from "./internal/ElementIdsCache";
@@ -15,10 +13,10 @@ import { SubjectModelIdsCache } from "./internal/SubjectModelIdsCache";
 import { VisibilityStateHandler } from "./internal/VisibilityStateHandler";
 import * as NodeUtils from "./NodeUtils";
 
+import type { ECClassGroupingNodeKey, GroupingNodeKey, NodeKey } from "@itwin/presentation-common";
 import type { TreeNodeItem } from "@itwin/components-react";
 import type { Id64String } from "@itwin/core-bentley";
 import type { Viewport } from "@itwin/core-frontend";
-import type { ECClassGroupingNodeKey, GroupingNodeKey } from "@itwin/presentation-common";
 import type { IFilteredPresentationTreeDataProvider } from "@itwin/presentation-components";
 import type { Observable } from "rxjs";
 import type { IVisibilityHandler, VisibilityChangeListener, VisibilityStatus } from "../VisibilityTreeEventHandler";
@@ -61,8 +59,7 @@ export class ModelsVisibilityHandler implements IVisibilityHandler {
   private _props: ModelsVisibilityHandlerProps;
   private _pendingVisibilityChange: any | undefined;
   private _subjectModelIdsCache: SubjectModelIdsCache;
-  private _visibilityStatusRetriever: VisibilityStateHandler;
-  private _filteredDataProvider?: IFilteredPresentationTreeDataProvider;
+  private _visibilityStateHandler: VisibilityStateHandler;
   private _elementIdsCache: ElementIdsCache;
   private _listeners = new Array<() => void>();
 
@@ -70,7 +67,7 @@ export class ModelsVisibilityHandler implements IVisibilityHandler {
     this._props = props;
     this._subjectModelIdsCache = props.subjectModelIdsCache ?? new SubjectModelIdsCache(this._props.viewport.iModel);
     this._elementIdsCache = new ElementIdsCache(this._props.viewport.iModel, this._props.rulesetId);
-    this._visibilityStatusRetriever = new VisibilityStateHandler({
+    this._visibilityStateHandler = new VisibilityStateHandler({
       viewport: this._props.viewport,
       elementIdsCache: this._elementIdsCache,
       subjectModelIdsCache: this._subjectModelIdsCache,
@@ -94,7 +91,7 @@ export class ModelsVisibilityHandler implements IVisibilityHandler {
 
   /** Sets data provider that is used to get filtered tree hierarchy. */
   public setFilteredDataProvider(provider: IFilteredPresentationTreeDataProvider | undefined) {
-    this._filteredDataProvider = provider;
+    this._visibilityStateHandler.filteredDataProvider = provider;
   }
 
   public static getNodeType(item: TreeNodeItem) {
@@ -103,7 +100,7 @@ export class ModelsVisibilityHandler implements IVisibilityHandler {
 
   /** Returns visibility status of the tree node. */
   public getVisibilityStatus(node: TreeNodeItem): VisibilityStatus | Promise<VisibilityStatus> {
-    const result = this._visibilityStatusRetriever.getVisibilityStatus(node);
+    const result = this._visibilityStateHandler.getVisibilityStatus(node);
     if (isObservable(result)) {
       return firstValueFrom(result);
     }
@@ -112,135 +109,51 @@ export class ModelsVisibilityHandler implements IVisibilityHandler {
 
   /** Changes visibility of the items represented by the tree node. */
   public async changeVisibility(node: TreeNodeItem, on: boolean) {
-    if (!isPresentationTreeNodeItem(node)) {
-      return;
-    }
-    const nodeKey = node.key;
-
-    if (NodeKey.isClassGroupingNodeKey(nodeKey)) {
-      await this.changeElementGroupingNodeState(nodeKey, on);
-      return;
-    }
-
-    if (!NodeKey.isInstancesNodeKey(nodeKey)) {
-      return;
-    }
-
-    if (NodeUtils.isSubjectNode(node)) {
-      await this.changeSubjectNodeState(
-        nodeKey.instanceKeys.map((key) => key.id),
-        node,
-        on,
-      );
-    } else if (NodeUtils.isModelNode(node)) {
-      await this.changeModelState(nodeKey.instanceKeys[0].id, on);
-    } else if (NodeUtils.isCategoryNode(node)) {
-      this.changeCategoryState(nodeKey.instanceKeys[0].id, this.getCategoryParentModelId(node), on);
-    } else {
-      await this.changeElementState(nodeKey.instanceKeys[0].id, this.getElementModelId(node), this.getElementCategoryId(node), on, node.hasChildren);
-    }
+    return toVoidPromise(this._visibilityStateHandler.changeVisibility(node, on));
   }
 
   protected async getSubjectNodeVisibility(ids: Id64String[], node: TreeNodeItem): Promise<VisibilityStatus> {
-    return firstValueFrom(this._visibilityStatusRetriever.getSubjectNodeVisibility(node, from(ids)));
+    return firstValueFrom(this._visibilityStateHandler.getSubjectNodeVisibility(node, from(ids)));
   }
 
   protected getModelDisplayStatus(id: Id64String): VisibilityStatus {
-    return this._visibilityStatusRetriever.getModelDisplayStatus(id);
+    return this._visibilityStateHandler.getModelDisplayStatus(id);
   }
 
   protected getCategoryDisplayStatus(id: Id64String, parentModelId: Id64String | undefined): VisibilityStatus {
-    return this._visibilityStatusRetriever.getCategoryDisplayStatus(id, parentModelId);
+    return this._visibilityStateHandler.getCategoryDisplayStatus(id, parentModelId);
   }
 
   protected async getElementGroupingNodeDisplayStatus(_id: string, key: ECClassGroupingNodeKey): Promise<VisibilityStatus> {
-    return firstValueFrom(this._visibilityStatusRetriever.getElementGroupingNodeDisplayStatus(key));
+    return firstValueFrom(this._visibilityStateHandler.getElementGroupingNodeDisplayStatus(key));
   }
 
   protected getElementDisplayStatus(elementId: Id64String, modelId: Id64String | undefined, categoryId: Id64String | undefined): VisibilityStatus {
-    return this._visibilityStatusRetriever.getElementDisplayStatus(elementId, modelId, categoryId);
+    return this._visibilityStateHandler.getElementDisplayStatus(elementId, modelId, categoryId);
   }
 
   protected async changeSubjectNodeState(ids: Id64String[], node: TreeNodeItem, on: boolean) {
-    if (!this._props.viewport.view.isSpatialView()) {
-      return;
-    }
-
-    if (this._filteredDataProvider) {
-      return this.changeFilteredSubjectState(this._filteredDataProvider, ids, node, on);
-    }
-
-    return this.changeSubjectState(ids, on);
-  }
-
-  private async changeFilteredSubjectState(provider: IFilteredPresentationTreeDataProvider, ids: Id64String[], node: TreeNodeItem, on: boolean) {
-    if (provider.nodeMatchesFilter(node)) {
-      return this.changeSubjectState(ids, on);
-    }
-
-    const children = await provider.getNodes(node);
-    return Promise.all(children.map(async (childNode) => this.changeVisibility(childNode, on)));
-  }
-
-  private async changeSubjectState(ids: Id64String[], on: boolean) {
-    const modelIdsObs = from(ids).pipe(this.getSubjectModelIds, toArray());
-    const modelIds = await lastValueFrom(modelIdsObs);
-    return this.changeModelsVisibility(modelIds, on);
+    return toVoidPromise(this._visibilityStateHandler.changeSubjectNodeState(from(ids), node, on));
   }
 
   protected async changeModelState(id: Id64String, on: boolean) {
-    if (!this._props.viewport.view.isSpatialView()) {
-      return;
-    }
-
-    return this.changeModelsVisibility([id], on);
+    return toVoidPromise(this._visibilityStateHandler.changeModelState(id, on));
   }
 
   protected async changeModelsVisibility(ids: Id64String[], visible: boolean) {
-    if (visible) {
-      return this._props.viewport.addViewedModels(ids);
-    } else {
-      this._props.viewport.changeModelDisplay(ids, false);
-    }
+    return toVoidPromise(this._visibilityStateHandler.changeModelsVisibility(ids, visible));
   }
 
   protected changeCategoryState(categoryId: Id64String, parentModelId: Id64String | undefined, on: boolean) {
-    if (parentModelId) {
-      const isDisplayedInSelector = this._props.viewport.view.viewsCategory(categoryId);
-      const ovr =
-        on === isDisplayedInSelector
-          ? PerModelCategoryVisibility.Override.None
-          : on
-            ? PerModelCategoryVisibility.Override.Show
-            : PerModelCategoryVisibility.Override.Hide;
-      this._props.viewport.perModelCategoryVisibility.setOverride(parentModelId, categoryId, ovr);
-      if (ovr === PerModelCategoryVisibility.Override.None && on) {
-        // we took off the override which means the category is displayed in selector, but
-        // doesn't mean all its subcategories are displayed - this call ensures that
-        this._props.viewport.changeCategoryDisplay([categoryId], true, true);
-      }
-      return;
-    }
-    this._props.viewport.changeCategoryDisplay([categoryId], on, on ? true : false);
+    return this._visibilityStateHandler.changeCategoryState(categoryId, parentModelId, on);
   }
 
   protected async changeElementGroupingNodeState(key: ECClassGroupingNodeKey, on: boolean) {
-    const { modelId, categoryId, elementIds } = await this.getGroupedElementIds(key);
-    await this.changeElementsState(modelId, categoryId, elementIds.getElementIds(), on);
+    return toVoidPromise(this._visibilityStateHandler.changeElementGroupingNodeState(key, on));
   }
 
   protected async changeElementState(id: Id64String, modelId: Id64String | undefined, categoryId: Id64String | undefined, on: boolean, hasChildren?: boolean) {
-    const isDisplayedByDefault = this.isElementDisplayedByDefault(modelId, categoryId);
-    const isHiddenDueToExclusiveAlwaysDrawnElements = this.hasExclusiveAlwaysDrawnElements();
-    this.changeElementStateInternal(id, on, isDisplayedByDefault, isHiddenDueToExclusiveAlwaysDrawnElements);
-    if (!hasChildren) {
-      return;
-    }
-
-    const childIdsContainer = this._elementIdsCache.getAssemblyElementIds(id);
-    for await (const childId of childIdsContainer.getElementIds()) {
-      this.changeElementStateInternal(childId, on, isDisplayedByDefault, isHiddenDueToExclusiveAlwaysDrawnElements);
-    }
+    return toVoidPromise(this._visibilityStateHandler.changeElementState(id, modelId, categoryId, on, hasChildren));
   }
 
   protected async changeElementsState(
@@ -249,49 +162,7 @@ export class ModelsVisibilityHandler implements IVisibilityHandler {
     elementIds: AsyncGenerator<Id64String>,
     on: boolean,
   ) {
-    const isDisplayedByDefault = this.isElementDisplayedByDefault(modelId, categoryId);
-    const isHiddenDueToExclusiveAlwaysDrawnElements = this.hasExclusiveAlwaysDrawnElements();
-    for await (const elementId of elementIds) {
-      this.changeElementStateInternal(elementId, on, isDisplayedByDefault, isHiddenDueToExclusiveAlwaysDrawnElements);
-    }
-  }
-
-  private changeElementStateInternal(elementId: Id64String, on: boolean, isDisplayedByDefault: boolean, isHiddenDueToExclusiveAlwaysDrawnElements: boolean) {
-    let changedNeverDraw = false;
-    let changedAlwaysDrawn = false;
-
-    const currNeverDrawn = new Set(this._props.viewport.neverDrawn ? this._props.viewport.neverDrawn : []);
-    const currAlwaysDrawn = new Set(this._props.viewport.alwaysDrawn ? this._props.viewport.alwaysDrawn : /* istanbul ignore next */ []);
-
-    if (on) {
-      changedNeverDraw = changedNeverDraw || currNeverDrawn.delete(elementId);
-      if ((!isDisplayedByDefault || isHiddenDueToExclusiveAlwaysDrawnElements) && !currAlwaysDrawn.has(elementId)) {
-        currAlwaysDrawn.add(elementId);
-        changedAlwaysDrawn = true;
-      }
-    } else {
-      changedAlwaysDrawn = changedAlwaysDrawn || currAlwaysDrawn.delete(elementId);
-      if (isDisplayedByDefault && !isHiddenDueToExclusiveAlwaysDrawnElements && !currNeverDrawn.has(elementId)) {
-        currNeverDrawn.add(elementId);
-        changedNeverDraw = true;
-      }
-    }
-
-    changedNeverDraw && this._props.viewport.setNeverDrawn(currNeverDrawn);
-    changedAlwaysDrawn && this._props.viewport.setAlwaysDrawn(currAlwaysDrawn, this._props.viewport.isAlwaysDrawnExclusive);
-  }
-
-  private isElementDisplayedByDefault(modelId: Id64String | undefined, categoryId: Id64String | undefined) {
-    return (
-      !!modelId &&
-      this.getModelDisplayStatus(modelId).state === "visible" &&
-      !!categoryId &&
-      this.getCategoryDisplayStatus(categoryId, modelId).state === "visible"
-    );
-  }
-
-  private hasExclusiveAlwaysDrawnElements() {
-    return this._props.viewport.isAlwaysDrawnExclusive && 0 !== this._props.viewport.alwaysDrawn?.size;
+    return toVoidPromise(from(elementIds).pipe(this._visibilityStateHandler.changeElementsState(modelId, categoryId, on)));
   }
 
   private onVisibilityChangeInternal() {
@@ -410,4 +281,15 @@ export async function toggleModels(models: string[], enable: boolean, viewport: 
  */
 export function areAllModelsVisible(models: string[], viewport: Viewport) {
   return models.length !== 0 ? models.every((id) => viewport.viewsModel(id)) : false;
+}
+
+/** Same as `firstValueFrom` except it won't throw if the observable emits no values. */
+async function toVoidPromise(obs: Observable<void> | Observable<undefined>): Promise<void> {
+  return new Promise((resolve, reject) => {
+    obs.subscribe({
+      next: resolve,
+      complete: resolve,
+      error: reject,
+    });
+  });
 }
--- packages/itwin/tree-widget/src/components/trees/models-tree/internal/VisibilityStateHandler.ts
+++ packages/itwin/tree-widget/src/components/trees/models-tree/internal/VisibilityStateHandler.ts
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
-import { defer, every, forkJoin, from, map, mergeAll, mergeMap, of, reduce } from "rxjs";
+import { concat, defer, EMPTY, every, forkJoin, from, iif, map, mergeAll, mergeMap, of, reduce, toArray } from "rxjs";
 import { PerModelCategoryVisibility } from "@itwin/core-frontend";
 import { NodeKey } from "@itwin/presentation-common";
 import { isPresentationTreeNodeItem } from "@itwin/presentation-components";
@@ -18,7 +18,7 @@ import type { Viewport } from "@itwin/core-frontend";
 import type { IFilteredPresentationTreeDataProvider } from "@itwin/presentation-components";
 import type { VisibilityStatus } from "../../VisibilityTreeEventHandler";
 import type { TreeNodeItem } from "@itwin/components-react";
-import type { Observable } from "rxjs";
+import type { Observable, OperatorFunction } from "rxjs";
 /**
  * Props for [[ModelsVisibilityHandler]]
  */
@@ -195,7 +195,8 @@ export class VisibilityStateHandler {
       if (this._props.viewport.alwaysDrawn.has(elementId)) {
         return createVisibilityStatus("visible", "element.displayedThroughAlwaysDrawnList");
       }
-      if (this._props.viewport.alwaysDrawn.size !== 0 && this._props.viewport.isAlwaysDrawnExclusive) {
+
+      if (this.hasExclusiveAlwaysDrawnElements()) {
         return createVisibilityStatus("hidden", "element.hiddenDueToOtherElementsExclusivelyAlwaysDrawn");
       }
     }
@@ -203,6 +204,220 @@ export class VisibilityStateHandler {
       return createVisibilityStatus("visible", "visible");
     }
     return createVisibilityStatus("hidden", "element.hiddenThroughCategory");
+  }
+
+  /** Changes visibility of the items represented by the tree node. */
+  public changeVisibility(node: TreeNodeItem, on: boolean): Observable<void> {
+    if (!isPresentationTreeNodeItem(node)) {
+      return EMPTY;
+    }
+    const nodeKey = node.key;
+
+    if (NodeKey.isClassGroupingNodeKey(nodeKey)) {
+      return this.changeElementGroupingNodeState(nodeKey, on);
+    }
+
+    if (!NodeKey.isInstancesNodeKey(nodeKey)) {
+      return EMPTY;
+    }
+
+    if (isSubjectNode(node)) {
+      return this.changeSubjectNodeState(from(nodeKey.instanceKeys).pipe(map((key) => key.id)), node, on);
+    }
+
+    if (isModelNode(node)) {
+      return this.changeModelState(nodeKey.instanceKeys[0].id, on);
+    }
+
+    if (isCategoryNode(node)) {
+      this.changeCategoryState(nodeKey.instanceKeys[0].id, getCategoryParentModelId(node), on);
+      return EMPTY;
+    }
+
+    return this.changeElementState(nodeKey.instanceKeys[0].id, getElementModelId(node), getElementCategoryId(node), on, node.hasChildren);
+  }
+
+  public changeSubjectNodeState(ids: Observable<Id64String>, node: TreeNodeItem, on: boolean): Observable<void> {
+    if (!this._props.viewport.view.isSpatialView()) {
+      return EMPTY;
+    }
+
+    if (this._filteredDataProvider) {
+      return this.changeFilteredSubjectState(this._filteredDataProvider, ids, node, on);
+    }
+
+    return this.changeSubjectState(ids, on);
+  }
+
+  private changeFilteredSubjectState(
+    provider: IFilteredPresentationTreeDataProvider,
+    ids: Observable<Id64String>,
+    node: TreeNodeItem,
+    on: boolean,
+  ): Observable<void> {
+    if (provider.nodeMatchesFilter(node)) {
+      return this.changeSubjectState(ids, on);
+    }
+
+    return from(provider.getNodes()).pipe(
+      mergeAll(),
+      mergeMap((childNode) => this.changeVisibility(childNode, on)),
+    );
+  }
+
+  private changeSubjectState(ids: Observable<Id64String>, on: boolean): Observable<void> {
+    return ids.pipe(
+      this.getSubjectModelIds,
+      toArray(),
+      mergeMap((modelId) => this.changeModelsVisibility(modelId, on)),
+    );
+  }
+
+  public changeModelState(id: Id64String, on: boolean): Observable<void> {
+    if (!this._props.viewport.view.isSpatialView()) {
+      return EMPTY;
+    }
+
+    return this.changeModelsVisibility([id], on);
+  }
+
+  public changeModelsVisibility(ids: Id64String[], visible: boolean): Observable<void> {
+    if (visible) {
+      return from(this._props.viewport.addViewedModels(ids));
+    }
+
+    this._props.viewport.changeModelDisplay(ids, false);
+    return EMPTY;
+  }
+
+  public changeCategoryState(categoryId: Id64String, parentModelId: Id64String | undefined, on: boolean) {
+    if (!parentModelId) {
+      return this._props.viewport.changeCategoryDisplay([categoryId], on, on);
+    }
+
+    const isDisplayedInSelector = this._props.viewport.view.viewsCategory(categoryId);
+    const ovr =
+      on === isDisplayedInSelector
+        ? PerModelCategoryVisibility.Override.None
+        : on
+          ? PerModelCategoryVisibility.Override.Show
+          : PerModelCategoryVisibility.Override.Hide;
+    this._props.viewport.perModelCategoryVisibility.setOverride(parentModelId, categoryId, ovr);
+    if (ovr === PerModelCategoryVisibility.Override.None && on) {
+      // we took off the override which means the category is displayed in selector, but
+      // doesn't mean all its subcategories are displayed - this call ensures that
+      this._props.viewport.changeCategoryDisplay([categoryId], true, true);
+    }
+  }
+
+  public changeElementGroupingNodeState(key: ECClassGroupingNodeKey, on: boolean): Observable<void> {
+    return this._props.elementIdsCache.getGroupedElementIds(key).pipe(
+      mergeMap(({ modelId, categoryId, elementIds }) => {
+        return elementIds.pipe(this.changeElementsState(modelId, categoryId, on));
+      }),
+    );
+  }
+
+  /**
+   * Update visibility of an element and all it's child elements
+   * by adding them to the always/never drawn list.
+   */
+  public changeElementState(
+    id: Id64String,
+    modelId: Id64String | undefined,
+    categoryId: Id64String | undefined,
+    on: boolean,
+    hasChildren?: boolean,
+  ): Observable<void> {
+    return concat(
+      of(id).pipe(this.changeElementsState(modelId, categoryId, on)),
+      iif(() => !!hasChildren, this._props.elementIdsCache.getAssemblyElementIds(id).pipe(this.changeElementsState(modelId, categoryId, on)), EMPTY),
+    );
+  }
+
+  /** Update visibility of a shallow list of elements by adding them to the always/never drawn list. */
+  public changeElementsState(modelId: Id64String | undefined, categoryId: Id64String | undefined, on: boolean): OperatorFunction<string, void> {
+    const isDisplayedByDefault = this.isElementDisplayedByDefault(modelId, categoryId);
+    const isHiddenDueToExclusiveAlwaysDrawnElements = this.hasExclusiveAlwaysDrawnElements();
+    return this.updateAlwaysAndNeverDrawnLists({ isDisplayedByDefault, isHiddenDueToExclusiveAlwaysDrawnElements, on });
+  }
+
+  private updateAlwaysAndNeverDrawnLists(props: {
+    isDisplayedByDefault: boolean;
+    isHiddenDueToExclusiveAlwaysDrawnElements: boolean;
+    on: boolean;
+  }): OperatorFunction<string, void> {
+    return (elementIds: Observable<string>) => {
+      const { on, isDisplayedByDefault, isHiddenDueToExclusiveAlwaysDrawnElements } = props;
+      return elementIds.pipe(
+        reduce(
+          (acc, elementId) => {
+            if (on) {
+              acc.changedNeverDrawn ||= acc.neverDrawn.delete(elementId);
+              if ((!isDisplayedByDefault || isHiddenDueToExclusiveAlwaysDrawnElements) && !acc.alwaysDrawn.has(elementId)) {
+                acc.alwaysDrawn.add(elementId);
+                acc.changedAlwaysDrawn = true;
+              }
+            } else {
+              acc.changedAlwaysDrawn ||= acc.alwaysDrawn.delete(elementId);
+              if (isDisplayedByDefault && !isHiddenDueToExclusiveAlwaysDrawnElements && !acc.neverDrawn.has(elementId)) {
+                acc.neverDrawn.add(elementId);
+                acc.changedNeverDrawn = true;
+              }
+            }
+            return acc;
+          },
+          {
+            changedNeverDrawn: false,
+            changedAlwaysDrawn: false,
+            neverDrawn: new Set(this._props.viewport.neverDrawn || []),
+            alwaysDrawn: new Set(this._props.viewport.alwaysDrawn || []),
+          },
+        ),
+        map((state) => {
+          state.changedNeverDrawn && this._props.viewport.setNeverDrawn(state.neverDrawn);
+          state.changedAlwaysDrawn && this._props.viewport.setAlwaysDrawn(state.alwaysDrawn, this._props.viewport.isAlwaysDrawnExclusive);
+        }),
+      );
+    };
+  }
+
+  private changeElementStateInternal(elementId: Id64String, on: boolean, isDisplayedByDefault: boolean, isHiddenDueToExclusiveAlwaysDrawnElements: boolean) {
+    let changedNeverDraw = false;
+    let changedAlwaysDrawn = false;
+
+    const currNeverDrawn = new Set(this._props.viewport.neverDrawn ? this._props.viewport.neverDrawn : []);
+    const currAlwaysDrawn = new Set(this._props.viewport.alwaysDrawn ? this._props.viewport.alwaysDrawn : /* istanbul ignore next */ []);
+
+    if (on) {
+      changedNeverDraw = changedNeverDraw || currNeverDrawn.delete(elementId);
+      if ((!isDisplayedByDefault || isHiddenDueToExclusiveAlwaysDrawnElements) && !currAlwaysDrawn.has(elementId)) {
+        currAlwaysDrawn.add(elementId);
+        changedAlwaysDrawn = true;
+      }
+    } else {
+      changedAlwaysDrawn = changedAlwaysDrawn || currAlwaysDrawn.delete(elementId);
+      if (isDisplayedByDefault && !isHiddenDueToExclusiveAlwaysDrawnElements && !currNeverDrawn.has(elementId)) {
+        currNeverDrawn.add(elementId);
+        changedNeverDraw = true;
+      }
+    }
+
+    changedNeverDraw && this._props.viewport.setNeverDrawn(currNeverDrawn);
+    changedAlwaysDrawn && this._props.viewport.setAlwaysDrawn(currAlwaysDrawn, this._props.viewport.isAlwaysDrawnExclusive);
+  }
+
+  private isElementDisplayedByDefault(modelId: Id64String | undefined, categoryId: Id64String | undefined) {
+    return (
+      !!modelId &&
+      this.getModelDisplayStatus(modelId).state === "visible" &&
+      !!categoryId &&
+      this.getCategoryDisplayStatus(categoryId, modelId).state === "visible"
+    );
+  }
+
+  private hasExclusiveAlwaysDrawnElements() {
+    return this._props.viewport.isAlwaysDrawnExclusive && 0 !== this._props.viewport.alwaysDrawn?.size;
   }
 
   private getSubjectModelIds(subjectIds: Observable<Id64String>): Observable<Id64String> {
