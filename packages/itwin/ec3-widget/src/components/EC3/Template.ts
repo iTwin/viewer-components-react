98ec46cf05c8fe92bd952bf131f1d03a2d53a729
Guillar1 <Guillar1@users.noreply.github.com>
2024-04-22 18:03:00 -0700

    removed repeated types

--- packages/itwin/ec3-widget/src/components/EC3/Template.ts
+++ packages/itwin/ec3-widget/src/components/EC3/Template.ts
@@ -2,65 +2,16 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-import type { EC3ConfigurationCreate, EC3ConfigurationUpdate } from "@itwin/insights-client";
+import type { EC3ConfigurationLabel } from "@itwin/insights-client";
 
+/**
+ * Template configuration
+ * @public
+ */
 export interface Configuration {
   displayName: string;
   description: string;
   id?: string;
   reportId?: string;
-  labels: Label[];
-}
-
-export interface Label {
-  reportTable: string;
-  name: string;
-  elementNameColumn: string;
-  elementQuantityColumn: string;
-  materials: Material[];
-}
-
-export interface Material {
-  nameColumn: string | undefined;
-}
-
-export function convertConfigurationCreate(childTemplate: Configuration): EC3ConfigurationCreate {
-  return {
-    reportId: childTemplate.reportId!,
-    displayName: childTemplate.displayName,
-    description: childTemplate.description,
-    labels: childTemplate.labels.map((x) => {
-      return {
-        name: x.name,
-        reportTable: x.reportTable,
-        elementNameColumn: x.elementNameColumn,
-        elementQuantityColumn: x.elementQuantityColumn,
-        materials: x.materials.map((m) => {
-          return {
-            nameColumn: m.nameColumn ?? "",
-          };
-        }),
-      };
-    }),
-  };
-}
-
-export function convertConfigurationUpdate(childTemplate: Configuration): EC3ConfigurationUpdate {
-  return {
-    displayName: childTemplate.displayName,
-    description: childTemplate.description,
-    labels: childTemplate.labels.map((x) => {
-      return {
-        name: x.name,
-        reportTable: x.reportTable,
-        elementNameColumn: x.elementNameColumn,
-        elementQuantityColumn: x.elementQuantityColumn,
-        materials: x.materials.map((m) => {
-          return {
-            nameColumn: m.nameColumn ?? "",
-          };
-        }),
-      };
-    }),
-  };
+  labels: EC3ConfigurationLabel[];
 }
