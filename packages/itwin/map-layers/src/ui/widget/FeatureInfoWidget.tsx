5fa52c096f0b6cc70be0ec130e265f1b9b61e841
Michel D'Astous <mdastous-bentley@users.noreply.github.com>
2024-05-08 09:23:57 -0400

    Added 'Copy Text' option in the Map Layers Info widget

--- packages/itwin/map-layers/src/ui/widget/FeatureInfoWidget.tsx
+++ packages/itwin/map-layers/src/ui/widget/FeatureInfoWidget.tsx
@@ -3,9 +3,11 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import * as React from "react";
+import { PrimitiveValue, PropertyRecord } from "@itwin/appui-abstract";
 import { useActiveFrontstageDef, WidgetState } from "@itwin/appui-react";
 import { VirtualizedPropertyGridWithDataProvider } from "@itwin/components-react";
-import { FillCentered, Orientation, ResizableContainerObserver } from "@itwin/core-react";
+import { ContextMenuItem, FillCentered, GlobalContextMenu, Orientation, ResizableContainerObserver } from "@itwin/core-react";
+import { SvgCopy } from "@itwin/itwinui-icons-react";
 import { MapLayersUI } from "../../mapLayers";
 import { FeatureInfoUiItemsProvider } from "../FeatureInfoUiItemsProvider";
 import { MapFeatureInfoOptions } from "../Interfaces";
@@ -21,6 +23,12 @@ interface MapFeatureInfoWidgetProps {
   featureInfoOpts: MapFeatureInfoOptions;
 }
 
+interface ContextMenuInfo {
+  clientX: number;
+  clientY: number;
+  propertyRecord: PropertyRecord;
+}
+
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function MapFeatureInfoWidget({ featureInfoOpts }: MapFeatureInfoWidgetProps) {
 
@@ -30,6 +38,9 @@ export function MapFeatureInfoWidget({ featureInfoOpts }: MapFeatureInfoWidgetPr
   const [noRecordsMessage] = React.useState(MapLayersUI.localization.getLocalizedString("mapLayers:FeatureInfoWidget.NoRecords"));
 
   const [{ width, height }, setSize] = React.useState({ width: 0, height: 0 });
+
+  const [contextMenuInfo, setContextMenuInfo] = React.useState<ContextMenuInfo | undefined>();
+  const closeContextMenu = () => setContextMenuInfo(undefined);
 
   const widgetDef = useSpecificWidgetDef(FeatureInfoUiItemsProvider.widgetId);
   const handleDataChanged = React.useCallback(() => {
@@ -60,15 +71,39 @@ export function MapFeatureInfoWidget({ featureInfoOpts }: MapFeatureInfoWidgetPr
 
   if (hasData && dataProvider.current) {
     return (
-      <ResizableContainerObserver onResize={handleResize}>
-        <VirtualizedPropertyGridWithDataProvider
-          width={width}
-          height={height}
-          dataProvider={dataProvider.current}
-          orientation={Orientation.Vertical}
-          isPropertySelectionEnabled={featureInfoOpts?.propertyGridOptions?.isPropertySelectionEnabled}
-        />
-      </ResizableContainerObserver>
+      <>
+        <ResizableContainerObserver onResize={handleResize}>
+          <VirtualizedPropertyGridWithDataProvider
+            width={width}
+            height={height}
+            dataProvider={dataProvider.current}
+            orientation={Orientation.Vertical}
+            isPropertySelectionEnabled={featureInfoOpts?.propertyGridOptions?.isPropertySelectionEnabled}
+            onPropertyContextMenu={(args) => setContextMenuInfo({
+              propertyRecord: args.propertyRecord,
+              clientX: args.event.clientX,
+              clientY: args.event.clientY})}
+
+          />
+        </ResizableContainerObserver>
+        <GlobalContextMenu
+          opened={!!contextMenuInfo}
+          onOutsideClick={closeContextMenu}
+          onSelect={closeContextMenu}
+          x={contextMenuInfo?.clientX ?? 0}
+          y={contextMenuInfo?.clientY ?? 0}
+        >
+          <ContextMenuItem
+            icon={<SvgCopy />}
+            onSelect={()=>{
+              const value = contextMenuInfo?.propertyRecord.value;
+              if (value !== undefined && value.hasOwnProperty("displayValue") )
+                navigator.clipboard.writeText((value as PrimitiveValue).displayValue??"").catch((_) => { });
+            }}
+            label="Copy Text">Copy Text
+          </ContextMenuItem>
+        </GlobalContextMenu>
+      </>
     );
   } else {
     return (
