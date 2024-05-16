7e2c08ec9633642e50b529c2adbf53904a5ec89a
Dominykas Jasiunas <33428304+jasdom@users.noreply.github.com>
2024-05-16 12:20:29 +0300

    Remove no longer needed refs

--- packages/itwin/tree-widget/src/components/trees/experimental/common/UseMultiCheckboxHandler.ts
+++ packages/itwin/tree-widget/src/components/trees/experimental/common/UseMultiCheckboxHandler.ts
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { PresentationHierarchyNode, PresentationTreeNode, isPresentationHierarchyNode } from "@itwin/presentation-hierarchies-react";
-import { useEffect, useRef } from "react";
 
 interface UseMultiCheckboxHandlerProps {
   rootNodes: PresentationTreeNode[] | undefined;
@@ -19,17 +18,14 @@ interface UseMultiCheckboxHandlerResult {
 /** @internal */
 export function useMultiCheckboxHandler(props: UseMultiCheckboxHandlerProps): UseMultiCheckboxHandlerResult {
   const { rootNodes, isNodeSelected, onClick } = props;
-  const rootNodesRef = useLatest(rootNodes);
-  const isNodeSelectedRef = useLatest(isNodeSelected);
-  const onClickRef = useLatest(onClick);
 
   const clickSelectedNodes = (nodes: Array<PresentationTreeNode>, checked: boolean) => {
     nodes.forEach((node) => {
       if (!isPresentationHierarchyNode(node)) {
         return;
       }
-      if (isNodeSelectedRef.current(node.id)) {
-        onClickRef.current(node, checked);
+      if (isNodeSelected(node.id)) {
+        onClick(node, checked);
       }
       if (node.isExpanded && typeof node.children !== "boolean") {
         clickSelectedNodes(node.children, checked);
@@ -38,21 +34,12 @@ export function useMultiCheckboxHandler(props: UseMultiCheckboxHandlerProps): Us
   };
 
   const onCheckboxClicked = (node: PresentationHierarchyNode, checked: boolean) => {
-    if (!isNodeSelectedRef.current(node.id)) {
-      onClickRef.current(node, checked);
+    if (!isNodeSelected(node.id)) {
+      onClick(node, checked);
       return;
     }
-    rootNodesRef.current && clickSelectedNodes(rootNodesRef.current, checked);
+    rootNodes && clickSelectedNodes(rootNodes, checked);
   };
 
   return { onCheckboxClicked };
-}
-
-export function useLatest<T>(value: T) {
-  const ref = useRef(value);
-  useEffect(() => {
-    ref.current = value;
-  }, [value]);
-
-  return ref;
 }
