35ea9b1981a9f360d91f96c2a174db6dfc254bd7
Daniel Rodriguez <DanRod1999@users.noreply.github.com>
2024-07-08 11:18:24 -0400

    copy rules and tags from original

--- packages/itwin/measure-tools/eslint.config.js
+++ packages/itwin/measure-tools/eslint.config.js
@@ -9,7 +9,16 @@ module.exports = [
     files: ["**/*.{ts,tsx}"],
     ...iTwinPlugin.configs.uiConfig,
     rules: {
-      "@itwin/no-internal": "error",
+      "@itwin/no-internal": [
+        "warn",
+        {
+          tag: [
+            "internal",
+            "alpha",
+            "beta",
+          ],
+        },
+      ],
       "@typescript-eslint/unbound-method": "off",
       "no-duplicate-imports": "off",
       "@typescript-eslint/consistent-type-imports": "error",
