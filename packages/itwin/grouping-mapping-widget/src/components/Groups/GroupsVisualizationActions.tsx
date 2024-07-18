146ef820f5f4c9edc35660874778a8fad2407dd8
tejassutar2024 <tejas.sutar@bentley.com>
2024-07-18 14:39:49 +0530

    Wrapped onClick function in useCallback

--- packages/itwin/grouping-mapping-widget/src/components/Groups/GroupsVisualizationActions.tsx
+++ packages/itwin/grouping-mapping-widget/src/components/Groups/GroupsVisualizationActions.tsx
@@ -3,7 +3,7 @@
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 import { ButtonGroup, IconButton, ToggleSwitch } from "@itwin/itwinui-react";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { ToggleGroupVisibility } from "./ToggleGroupVisibility";
 import { SvgVisibilityHide, SvgVisibilityShow } from "@itwin/itwinui-icons-react";
 import "./GroupsVisualizationActions.scss";
@@ -29,6 +29,15 @@ export const GroupVisualizationActions = ({
   const { showGroupColor, setShowGroupColor } = useGroupHilitedElementsContext();
   const [show, setShow] = useState<boolean>(false);
 
+  const onVisibilityClick = useCallback(() => {
+    if (show) {
+      showAll();
+    } else {
+      hideAll();
+    }
+    setShow(!show);
+  }, [show, showAll, hideAll]);
+
   return (
     <ButtonGroup className="gmw-visual-buttons">
       {!hideVisualizationToggle && (
@@ -36,15 +45,7 @@ export const GroupVisualizationActions = ({
       )}
       <ButtonGroup>
         <ToggleGroupVisibility isLoadingQuery={disabled} showGroupColor={showGroupColor} setShowGroupColor={setShowGroupColor} />
-        <IconButton
-          title={show ? "Show All" : "Hide All"}
-          onClick={() => {
-            show ? showAll() : hideAll();
-            setShow(!show);
-          }}
-          disabled={disabled}
-          styleType="borderless"
-        >
+        <IconButton title={show ? "Show All" : "Hide All"} onClick={onVisibilityClick} disabled={disabled} styleType="borderless">
           {show ? <SvgVisibilityShow /> : <SvgVisibilityHide />}
         </IconButton>
       </ButtonGroup>
