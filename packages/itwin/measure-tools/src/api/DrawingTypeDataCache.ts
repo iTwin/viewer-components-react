aea256b9bb25c705c28da1dee03b32d571045ff1
Maxime-Brassard <125395992+Maxime-Brassard@users.noreply.github.com>
2024-08-19 10:24:07 -0400

    Task/drawing type cache refactor (#992)
    * drawingTypeCache refactor
    
    * change file
    
    * email
    
    * added clear function
    
    * made it a singleton

--- packages/itwin/measure-tools/src/api/DrawingTypeDataCache.ts
+++ packages/itwin/measure-tools/src/api/DrawingTypeDataCache.ts
@@ -3,42 +3,95 @@
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
 
-import type { IModelConnection } from "@itwin/core-frontend";
+import type { Viewport } from "@itwin/core-frontend";
+import { IModelConnection } from "@itwin/core-frontend";
 import { IModelApp } from "@itwin/core-frontend";
 import { SheetMeasurementsHelper } from "./SheetMeasurementHelper";
+import type { Id64String } from "@itwin/core-bentley";
 
 export class DrawingDataCache {
 
-  private _drawingTypeCache: SheetMeasurementsHelper.DrawingTypeData[];
-  private _sheetChangeListener: VoidFunction[] = [];
+  private _drawingTypeCache: Map<IModelConnection, Map<Id64String, SheetMeasurementsHelper.DrawingTypeData[]>>;
+  private _viewportModelChangedListeners: Map<Viewport, () => void>;
+  private static _instance: DrawingDataCache | undefined;
 
-  public get drawingtypes(): ReadonlyArray<SheetMeasurementsHelper.DrawingTypeData> {
-    return this._drawingTypeCache;
+  private constructor() {
+    this._drawingTypeCache = new Map<IModelConnection, Map<Id64String, SheetMeasurementsHelper.DrawingTypeData[]>>();
+    this._viewportModelChangedListeners = new Map<Viewport, () => void>();
+
+    this.setupEvents();
+
+    // Populate initial viewports
+    for (const vp of IModelApp.viewManager)
+      this.addViewport(vp);
   }
 
-  public constructor() {
-    this._drawingTypeCache = [];
+  public static getInstance(): DrawingDataCache {
+    if (DrawingDataCache._instance === undefined) {
+       DrawingDataCache._instance = new DrawingDataCache();
+    }
+    return DrawingDataCache._instance;
   }
 
-  public async updateDrawingTypeCache(iModel: IModelConnection) {
-    this._sheetChangeListener.forEach((func) => {
-      func();
+  private setupEvents() {
+    // If an imodel closes, clear the cache for it
+    IModelConnection.onClose.addListener((imodel) => {
+      this._drawingTypeCache.delete(imodel);
     });
-    this._sheetChangeListener = [];
-    this._drawingTypeCache = [];
 
-    const sheetIds = new Set<string>();
+    // Listen for new viewports opening
+    IModelApp.viewManager.onViewOpen.addListener((vp) => {
+      this.addViewport(vp);
+    });
 
-    for (const viewport of IModelApp.viewManager) {
-      if (viewport.view.isSheetView()) {
-        this._sheetChangeListener.push(viewport.onViewedModelsChanged.addListener(async () => this.updateDrawingTypeCache(iModel)));
-        sheetIds.add(viewport.view.id);
-      }
+    // Listen for viewports closing, this also is called when IModelApp shuts down
+    IModelApp.viewManager.onViewClose.addListener((vp) => {
+      this.dropViewport(vp);
+    });
+  }
+
+  private addViewport(vp: Viewport) {
+    vp.onViewedModelsChanged.addListener((viewport) =>{
+      if (!viewport.view.isSheetView())
+        return;
+
+      void this.querySheetDrawingData(viewport.iModel, viewport.view.id);
+    });
+  }
+
+  private dropViewport(vp: Viewport) {
+    const listener = this._viewportModelChangedListeners.get(vp);
+    if (listener) {
+      listener();
+      this._viewportModelChangedListeners.delete(vp);
+    }
+  }
+
+  public getSheetDrawingDataForViewport(vp: Viewport): ReadonlyArray<SheetMeasurementsHelper.DrawingTypeData> {
+    if (!vp.view.isSheetView())
+      return [];
+
+    const cache = this._drawingTypeCache.get(vp.iModel);
+    if (cache)
+      return cache.get(vp.view.id) ?? [];
+
+    return [];
+  }
+
+  public async querySheetDrawingData(imodel: IModelConnection, viewedModelID: string): Promise<ReadonlyArray<SheetMeasurementsHelper.DrawingTypeData>> {
+    let cache = this._drawingTypeCache.get(imodel);
+    if (!cache) {
+      cache = new Map<Id64String, SheetMeasurementsHelper.DrawingTypeData[]>();
+      this._drawingTypeCache.set(imodel, cache);
     }
 
-    for (const id of sheetIds) {
-      this._drawingTypeCache = this._drawingTypeCache.concat(await SheetMeasurementsHelper.getSheetTypes(iModel, id));
+    let sheetData = cache.get(viewedModelID);
+    if (!sheetData) {
+      sheetData = await SheetMeasurementsHelper.getSheetTypes(imodel, viewedModelID);
+      cache.set(viewedModelID, sheetData);
     }
+
+    return sheetData;
   }
 
 }
