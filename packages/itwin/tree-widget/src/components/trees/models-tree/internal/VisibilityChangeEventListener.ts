d6afce1fe31d1fbc86415bf2830b76db01f5d6bb
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2024-11-26 12:40:24 +0200

    Add ability to suppress visibility change events

--- packages/itwin/tree-widget/src/components/trees/models-tree/internal/VisibilityChangeEventListener.ts
+++ packages/itwin/tree-widget/src/components/trees/models-tree/internal/VisibilityChangeEventListener.ts
@@ -11,14 +11,17 @@ import type { Viewport } from "@itwin/core-frontend";
 /** @internal */
 export interface IVisibilityChangeEventListener extends IDisposable {
   onVisibilityChange: BeEvent<() => void>;
+  suppressChangeEvents(): void;
+  unSuppressChangeEvents(): void;
 }
 
 /** @internal */
 export function createVisibilityChangeEventListener(viewport: Viewport): IVisibilityChangeEventListener {
   const onVisibilityChange = new BeEvent<() => void>();
   let pendingVisibilityChange: undefined | ReturnType<typeof setTimeout>;
+  let suppressChangeEvents: number = 0;
   const handleVisibilityChange = () => {
-    if (pendingVisibilityChange) {
+    if (pendingVisibilityChange || suppressChangeEvents > 0) {
       return;
     }
     pendingVisibilityChange = setTimeout(() => {
@@ -45,6 +48,15 @@ export function createVisibilityChangeEventListener(viewport: Viewport): IVisibi
       }
       listeners.forEach((x) => x());
       listeners.length = 0;
+    },
+    suppressChangeEvents: () => {
+      suppressChangeEvents++;
+    },
+    unSuppressChangeEvents: () => {
+      suppressChangeEvents--;
+      if (suppressChangeEvents === 0) {
+        handleVisibilityChange();
+      }
     },
   };
 }
