1665f7785a0a740fcd182284b3ae581cbf635f0f
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2024-12-05 08:34:30 +0200

    don't require auth env variables when building test-viewer for e2e

--- apps/test-viewer/src/vite-env.d.ts
+++ apps/test-viewer/src/vite-env.d.ts
@@ -6,6 +6,7 @@
 
 interface ImportMetaEnv {
   readonly IMJS_GLOBAL_PROPS?: string;
+  readonly IMJS_BUILD_MODE?: string;
   readonly IMJS_URL_PREFIX?: string;
   readonly IMJS_ITWIN_ID?: string;
   readonly IMJS_IMODEL_ID?: string;
