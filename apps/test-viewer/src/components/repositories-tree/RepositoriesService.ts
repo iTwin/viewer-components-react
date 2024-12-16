bff18e59c75113b71b2009a75515cc3dcd5206bf
Martynas <43886789+MartynasStrazdas@users.noreply.github.com>
2024-12-16 15:59:55 +0200

    [tree-widget]: repository tree investigation (#1104)
    * initial repository tree
    
    * Update packages/itwin/tree-widget/src/components/trees/repositories-tree/UseRepositoriesHierarchyProvider.ts
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * Update packages/itwin/tree-widget/src/components/trees/repositories-tree/UseRepositoriesHierarchyProvider.ts
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * Update packages/itwin/tree-widget/src/components/trees/repositories-tree/FormatLabel.tsx
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * adjust provider for hierarchy-react 1.2
    
    * Update apps/test-viewer/src/UiProvidersConfig.tsx
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * fixes
    
    * access token adjustments
    
    * Update apps/test-viewer/src/components/repositories-tree/RepositoriesService.ts
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    * partial fixes
    
    * aditional changes
    
    * partial fixes
    
    * adjustments
    
    * adjustment
    
    * url prop change
    
    * merge fix
    
    * lint fixes
    
    * update e2e tests
    
    * fix
    
    ---------
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>

---
+++ apps/test-viewer/src/components/repositories-tree/RepositoriesService.ts
@@ -0,0 +1,70 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import { IModelApp } from "@itwin/core-frontend";
+import type { ITwinRepositoryType } from "./RepositoriesType";
+
+interface RepositoryData {
+  id?: string;
+  displayName: string;
+  type?: string;
+  name?: string;
+}
+
+interface ITwinRepository {
+  class: ITwinRepositoryType;
+  subClass?: string;
+  uri: string;
+}
+
+/**
+ * @internal
+ */
+export async function getItwinRepositories(itwinId: string, baseUrl?: string): Promise<ITwinRepository[]> {
+  const url = getRepositoriesUrl(itwinId, baseUrl);
+  const result = (await fetchData(url)) as ITwinRepository[];
+  return result;
+}
+
+/**
+ * @internal
+ */
+export async function getRepositoryData(url: string): Promise<RepositoryData[]> {
+  const result = (await fetchData(url)) as RepositoryData[];
+  return result;
+}
+
+async function fetchData(url: string): Promise<unknown> {
+  const accessToken = await IModelApp.getAccessToken();
+  const headers = {
+    Authorization: accessToken,
+    "Content-Type": "application/json",
+    Prefer: "return=representation",
+  };
+
+  try {
+    const response = await fetch(url, {
+      headers,
+    });
+    const result = await response.json();
+
+    // get the data avoiding links
+    delete result._links;
+    const values = Object.values(result);
+    const data = values[0];
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+function getRepositoriesUrl(itwinId: string, baseUrl?: string) {
+  if (!baseUrl) {
+    return `https://api.bentley.com/itwins/${itwinId}/repositories`;
+  }
+
+  return baseUrl?.includes("https://") ? `${baseUrl}/itwins/${itwinId}/repositories` : `https://${baseUrl}/itwins/${itwinId}/repositories`;
+}
