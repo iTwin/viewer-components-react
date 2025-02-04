36bf1452c45e2cbef1e33356915b311c1f05c874
Grigas <35135765+grigasp@users.noreply.github.com>
2025-02-04 14:57:31 +0200

    Tree widget: Update to latest alphas (#1179)
    * Update deps to latest alphas
    
    * change

--- apps/test-viewer/.pnpmfile.cjs
+++ apps/test-viewer/.pnpmfile.cjs
@@ -3,7 +3,6 @@
 module.exports = {
   hooks: {
     readPackage(pkg, _context) {
-      // skipDeps(pkg);
       if (pkg.name === "@itwin/tree-widget-react") {
         pkg.dependencies["@itwin/itwinui-react"] = pkg.peerDependencies["@itwin/itwinui-react"];
         delete pkg.peerDependencies["@itwin/itwinui-react"];
