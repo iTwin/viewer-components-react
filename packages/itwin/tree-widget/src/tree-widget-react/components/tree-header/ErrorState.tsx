808ec1f09294557d8fcfb5db65c384b6c576587b
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-02-24 17:36:52 +0200

    Render error message on tree load error

---
+++ packages/itwin/tree-widget/src/tree-widget-react/components/tree-header/ErrorState.tsx
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Button, Icon, Text } from "@itwin/itwinui-react/bricks";
+import { TreeWidget } from "../../TreeWidget.js";
+
+import type { FallbackProps } from "react-error-boundary";
+
+const errorIcon = new URL("@itwin/itwinui-icons/status-error.svg", import.meta.url).href;
+
+/** @internal */
+export function ErrorState({ resetErrorBoundary }: FallbackProps) {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100%", gap: "0.5rem" }}>
+      <Icon href={errorIcon} size="large" />
+      <Text>{TreeWidget.translate("errorState.description")}</Text>
+      <Button onClick={resetErrorBoundary}>{TreeWidget.translate("errorState.retryButtonLabel")}</Button>
+    </div>
+  );
+}
