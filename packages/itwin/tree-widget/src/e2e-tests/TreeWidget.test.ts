d2f21268594bccf11974a0ef6c665016ddde4f42
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2025-02-26 11:09:40 +0200

    Skip tree selector e2e tests

--- packages/itwin/tree-widget/src/e2e-tests/TreeWidget.test.ts
+++ packages/itwin/tree-widget/src/e2e-tests/TreeWidget.test.ts
@@ -7,12 +7,15 @@ import type { Locator } from "@playwright/test";
 import { test } from "@playwright/test";
 import { initTreeWidgetTest, locateNode, selectTree, takeScreenshot } from "./utils.js";
 
-test.describe("Widget", () => {
+// Skipped because tree selector uses native `select` element. Expanded selected are rendered by OS and they are not
+// part of the page so playwright cannot see them when taking screenshot.
+// Same issue is in pupppeteer https://github.com/puppeteer/puppeteer/issues/1306
+test.describe.skip("Widget", () => {
   let treeWidget: Locator;
 
   test.beforeEach(async ({ page, baseURL }) => {
     treeWidget = await initTreeWidgetTest({ page, baseURL });
-    await locateNode(treeWidget, "ProcessPhysicalModel").waitFor();
+    await locateNode(treeWidget, "ProcessPhysicalModel").getByRole("button", { name: "Determining visibility..." }).waitFor({ state: "detached" });
   });
 
   test("tree selector", async ({ page }) => {
