08d798a3db4fe8ef543856aea3723c5e0e74928e
Grigas Petraitis <35135765+grigasp@users.noreply.github.com>
2025-05-26 12:01:49 +0300

    Fix loading children for classification tables and classifications

--- packages/itwin/tree-widget/src/tree-widget-react/components/trees/classifications-tree/ClassificationsTreeDefinition.ts
+++ packages/itwin/tree-widget/src/tree-widget-react/components/trees/classifications-tree/ClassificationsTreeDefinition.ts
@@ -141,39 +141,41 @@ export class ClassificationsTreeDefinition implements HierarchyDefinition {
       contentClass: { fullName: CLASS_NAME_Classification, alias: "this" },
     });
     const childrenInfo = await this._props.idsCache.getDirectCategoriesAndClassifications(classificationTableIds);
-    return [
-      {
-        fullClassName: CLASS_NAME_Classification,
-        query: {
-          ecsql: `
-            SELECT
-              ${await this._selectQueryFactory.createSelectClause({
-                ecClassId: { selector: ECSql.createRawPropertyValueSelector("this", "ECClassId") },
-                ecInstanceId: { selector: "this.ECInstanceId" },
-                nodeLabel: {
-                  selector: await this._nodeLabelSelectClauseFactory.createSelectClause({
-                    classAlias: "this",
-                    className: CLASS_NAME_Classification,
-                  }),
-                },
-                hasChildren: {
-                  selector: createClassificationHasChildrenSelector("this"),
-                },
-                extendedData: {
-                  type: "Classification",
-                },
-                supportsFiltering: true,
-              })}
-            FROM
-              ${instanceFilterClauses.from} this
-            ${instanceFilterClauses.joins}
-            WHERE
-              this.ECInstanceId IN (${childrenInfo.classificationIds.join(",")})
-              ${instanceFilterClauses.where ? `AND ${instanceFilterClauses.where}` : ""}
-          `,
-        },
-      },
-    ];
+    return childrenInfo.classificationIds.length
+      ? [
+          {
+            fullClassName: CLASS_NAME_Classification,
+            query: {
+              ecsql: `
+                SELECT
+                  ${await this._selectQueryFactory.createSelectClause({
+                    ecClassId: { selector: ECSql.createRawPropertyValueSelector("this", "ECClassId") },
+                    ecInstanceId: { selector: "this.ECInstanceId" },
+                    nodeLabel: {
+                      selector: await this._nodeLabelSelectClauseFactory.createSelectClause({
+                        classAlias: "this",
+                        className: CLASS_NAME_Classification,
+                      }),
+                    },
+                    hasChildren: {
+                      selector: createClassificationHasChildrenSelector("this"),
+                    },
+                    extendedData: {
+                      type: "Classification",
+                    },
+                    supportsFiltering: true,
+                  })}
+                FROM
+                  ${instanceFilterClauses.from} this
+                ${instanceFilterClauses.joins}
+                WHERE
+                  this.ECInstanceId IN (${childrenInfo.classificationIds.join(",")})
+                  ${instanceFilterClauses.where ? `AND ${instanceFilterClauses.where}` : ""}
+              `,
+            },
+          },
+        ]
+      : [];
   }
 
   async #createClassificationChildrenQuery(props: {
@@ -184,80 +186,88 @@ export class ClassificationsTreeDefinition implements HierarchyDefinition {
     const childrenInfo = await this._props.idsCache.getDirectCategoriesAndClassifications(classificationIds);
     return [
       // load child classifications
-      await (async () => {
-        const instanceFilterClauses = await this._selectQueryFactory.createFilterClauses({
-          filter: instanceFilter,
-          contentClass: { fullName: CLASS_NAME_Classification, alias: "this" },
-        });
-        return {
-          fullClassName: CLASS_NAME_Classification,
-          query: {
-            ecsql: `
-              SELECT
-                ${await this._selectQueryFactory.createSelectClause({
-                  ecClassId: { selector: ECSql.createRawPropertyValueSelector("this", "ECClassId") },
-                  ecInstanceId: { selector: "this.ECInstanceId" },
-                  nodeLabel: {
-                    selector: await this._nodeLabelSelectClauseFactory.createSelectClause({
-                      classAlias: "this",
-                      className: CLASS_NAME_Classification,
-                    }),
-                  },
-                  hasChildren: {
-                    selector: createClassificationHasChildrenSelector("this"),
-                  },
-                  extendedData: {
-                    type: "Classification",
-                  },
-                  supportsFiltering: true,
-                })}
-              FROM
-                ${instanceFilterClauses.from} this
-              ${instanceFilterClauses.joins}
-              WHERE
-                this.ECInstanceId IN (${childrenInfo.classificationIds.join(",")})
-                ${instanceFilterClauses.where ? `AND ${instanceFilterClauses.where}` : ""}
-            `,
-          },
-        };
-      })(),
+      ...(childrenInfo.classificationIds.length
+        ? [
+            await(async () => {
+              const instanceFilterClauses = await this._selectQueryFactory.createFilterClauses({
+                filter: instanceFilter,
+                contentClass: { fullName: CLASS_NAME_Classification, alias: "this" },
+              });
+              return {
+                fullClassName: CLASS_NAME_Classification,
+                query: {
+                  ecsql: `
+                    SELECT
+                      ${await this._selectQueryFactory.createSelectClause({
+                        ecClassId: { selector: ECSql.createRawPropertyValueSelector("this", "ECClassId") },
+                        ecInstanceId: { selector: "this.ECInstanceId" },
+                        nodeLabel: {
+                          selector: await this._nodeLabelSelectClauseFactory.createSelectClause({
+                            classAlias: "this",
+                            className: CLASS_NAME_Classification,
+                          }),
+                        },
+                        hasChildren: {
+                          selector: createClassificationHasChildrenSelector("this"),
+                        },
+                        extendedData: {
+                          type: "Classification",
+                        },
+                        supportsFiltering: true,
+                      })}
+                    FROM
+                      ${instanceFilterClauses.from} this
+                    ${instanceFilterClauses.joins}
+                    WHERE
+                      this.ECInstanceId IN (${childrenInfo.classificationIds.join(",")})
+                      ${instanceFilterClauses.where ? `AND ${instanceFilterClauses.where}` : ""}
+                  `,
+                },
+              };
+            })(),
+          ]
+        : []),
       // load referenced categories
-      await (async () => {
-        const instanceFilterClauses = await this._selectQueryFactory.createFilterClauses({
-          filter: instanceFilter,
-          contentClass: { fullName: CLASS_NAME_Category, alias: "this" },
-        });
-        return {
-          fullClassName: CLASS_NAME_Category,
-          query: {
-            ecsql: `
-            SELECT
-              ${await this._selectQueryFactory.createSelectClause({
-                ecClassId: { selector: ECSql.createRawPropertyValueSelector("this", "ECClassId") },
-                ecInstanceId: { selector: "this.ECInstanceId" },
-                nodeLabel: {
-                  selector: await this._nodeLabelSelectClauseFactory.createSelectClause({
-                    classAlias: "this",
-                    className: CLASS_NAME_Category,
-                  }),
+      ...(childrenInfo.categoryIds.length
+        ? [
+            await(async () => {
+              const instanceFilterClauses = await this._selectQueryFactory.createFilterClauses({
+                filter: instanceFilter,
+                contentClass: { fullName: CLASS_NAME_Category, alias: "this" },
+              });
+              return {
+                fullClassName: CLASS_NAME_Category,
+                query: {
+                  ecsql: `
+                    SELECT
+                      ${await this._selectQueryFactory.createSelectClause({
+                        ecClassId: { selector: ECSql.createRawPropertyValueSelector("this", "ECClassId") },
+                        ecInstanceId: { selector: "this.ECInstanceId" },
+                        nodeLabel: {
+                          selector: await this._nodeLabelSelectClauseFactory.createSelectClause({
+                            classAlias: "this",
+                            className: CLASS_NAME_Category,
+                          }),
+                        },
+                        hasChildren: true, // we won't get the category if it doesn't have elements, and if it has elements, it will have children
+                        extendedData: {
+                          type: { selector: `IIF(ec_classname(this.ECClassId, 'c') = 'SpatialCategory', 'SpatialCategory', 'DrawingCategory')` },
+                          classificationIds: { selector: createIdsSelector(classificationIds) },
+                        },
+                        supportsFiltering: true,
+                      })}
+                    FROM
+                      ${instanceFilterClauses.from} this
+                    ${instanceFilterClauses.joins}
+                    WHERE
+                      this.ECInstanceId IN (${childrenInfo.categoryIds.join(",")})
+                      ${instanceFilterClauses.where ? `AND ${instanceFilterClauses.where}` : ""}
+                  `,
                 },
-                hasChildren: true, // we won't get the category if it doesn't have elements, and if it has elements, it will have children
-                extendedData: {
-                  type: { selector: `IIF(ec_classname(this.ECClassId, 'c') = 'SpatialCategory', 'SpatialCategory', 'DrawingCategory')` },
-                  classificationIds: { selector: createIdsSelector(classificationIds) },
-                },
-                supportsFiltering: true,
-              })}
-            FROM
-              ${instanceFilterClauses.from} this
-            ${instanceFilterClauses.joins}
-            WHERE
-              this.ECInstanceId IN (${childrenInfo.categoryIds.join(",")})
-              ${instanceFilterClauses.where ? `AND ${instanceFilterClauses.where}` : ""}
-          `,
-          },
-        };
-      })(),
+              };
+            })(),
+          ]
+        : []),
     ];
   }
 
