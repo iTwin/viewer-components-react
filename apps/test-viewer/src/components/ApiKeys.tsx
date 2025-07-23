8394f55100949c152f6061b3844a27e62986f7d5
JonasDov <100586436+JonasDov@users.noreply.github.com>
2025-07-23 14:02:23 +0300

    Add prettier to the pipeline (#1384)
    * Add prettier
    
    * Add changeset

--- apps/test-viewer/src/components/ApiKeys.tsx
+++ apps/test-viewer/src/components/ApiKeys.tsx
@@ -12,7 +12,6 @@ export class ApiKeys {
     return import.meta.env.IMJS_GOOGLE_MAPS_KEY ?? "";
   }
 
-
   public static get CesiumKey() {
     return import.meta.env.IMJS_CESIUM_ION_KEY ?? "";
   }
