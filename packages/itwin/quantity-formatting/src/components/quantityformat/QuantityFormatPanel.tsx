ea834a9e5e52840132ee2452eb7d91c1fcdb413a
Nam Le <50554904+hl662@users.noreply.github.com>
2025-08-13 14:01:06 -0400

    flesh out quantity format panel more

--- packages/itwin/quantity-formatting/src/components/quantityformat/QuantityFormatPanel.tsx
+++ packages/itwin/quantity-formatting/src/components/quantityformat/QuantityFormatPanel.tsx
@@ -7,8 +7,9 @@ import * as React from "react";
 import type { UnitProps, FormatDefinition } from "@itwin/core-quantity";
 import type { UnitsProvider } from "@itwin/core-quantity";
 import { FormatPanel } from "./FormatPanel.js";
-import { Divider } from "@itwin/itwinui-react";
+import { Button, ButtonGroup, Divider, Flex } from "@itwin/itwinui-react";
 import { FormatSample } from "./FormatSample.js";
+import { useTranslation } from "../../useTranslation.js";
 
 /** Properties of [[QuantityFormatPanel]] component.
  * @beta
@@ -31,19 +32,35 @@ export function QuantityFormatPanel(props: QuantityFormatPanelProps) {
     onFormatChange,
     showSample = true,
   } = props;
+
+  const { translate } = useTranslation();
+
+  // Clone the formatDefinition to work with internally
+  const [clonedFormatDefinition, setClonedFormatDefinition] = React.useState<FormatDefinition>(() =>
+  ({ ...formatDefinition })
+  );
+
+  const [_saveEnabled, _setSaveEnabled] = React.useState(false);
+
   const [persistenceUnit, setPersistenceUnit] = React.useState<
     UnitProps | undefined
   >(undefined);
+
+  // Update the cloned format definition when the prop changes
+  React.useEffect(() => {
+    setClonedFormatDefinition({ ...formatDefinition });
+    _setSaveEnabled(false);
+  }, [formatDefinition]);
 
   // Generate persistenceUnit from first composite unit
   React.useEffect(() => {
     const loadPersistenceUnit = async () => {
       if (
-        formatDefinition.composite &&
-        formatDefinition.composite.units &&
-        formatDefinition.composite.units.length > 0
+        clonedFormatDefinition.composite &&
+        clonedFormatDefinition.composite.units &&
+        clonedFormatDefinition.composite.units.length > 0
       ) {
-        const firstUnitName = formatDefinition.composite.units[0].name;
+        const firstUnitName = clonedFormatDefinition.composite.units[0].name;
         try {
           const unit = await unitsProvider.findUnitByName(firstUnitName);
           setPersistenceUnit(unit);
@@ -56,21 +73,32 @@ export function QuantityFormatPanel(props: QuantityFormatPanelProps) {
     };
 
     void loadPersistenceUnit();
-  }, [formatDefinition.composite, unitsProvider]);
+  }, [clonedFormatDefinition.composite, unitsProvider]);
 
   const handleOnFormatChanged = React.useCallback(
     async (newProps: FormatDefinition) => {
-      onFormatChange && onFormatChange(newProps);
+      setClonedFormatDefinition(newProps);
+      _setSaveEnabled(true);
     },
-    [onFormatChange]
+    []
   );
+
+  const handleSave = React.useCallback(() => {
+    onFormatChange && onFormatChange(clonedFormatDefinition);
+    _setSaveEnabled(false);
+  }, [onFormatChange, clonedFormatDefinition]);
+
+  const handleClear = React.useCallback(() => {
+    setClonedFormatDefinition({...formatDefinition});
+    _setSaveEnabled(false);
+  }, [formatDefinition]);
 
   return (
     <div className="components-quantityFormat-quantityPanel">
       {showSample && (
         <>
           <FormatSample
-            formatProps={formatDefinition}
+            formatProps={clonedFormatDefinition}
             unitsProvider={unitsProvider}
             persistenceUnit={persistenceUnit}
             initialMagnitude={props.initialMagnitude}
@@ -79,11 +107,22 @@ export function QuantityFormatPanel(props: QuantityFormatPanelProps) {
         </>
       )}
       <FormatPanel
-        formatProps={formatDefinition}
+        formatProps={clonedFormatDefinition}
         unitsProvider={unitsProvider}
         onFormatChange={handleOnFormatChanged}
         persistenceUnit={persistenceUnit}
       />
+      <Divider />
+      <Flex justifyContent="flex-end" gap="xs">
+        <ButtonGroup>
+          <Button styleType="default" onClick={handleClear} disabled={!_saveEnabled}>
+            {translate("QuantityFormat:labels.clear")}
+          </Button>
+          <Button styleType="high-visibility" onClick={handleSave} disabled={!_saveEnabled}>
+            {translate("QuantityFormat:labels.save")}
+          </Button>
+        </ButtonGroup>
+      </Flex>
     </div>
   );
 }
