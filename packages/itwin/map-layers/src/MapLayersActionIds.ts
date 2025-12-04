6791b98b778ae17dab24ab01a158973696f7d5b5
Jonas <100586436+JonasDov@users.noreply.github.com>
2025-12-04 14:15:52 +0200

    Delete all files

--- packages/itwin/map-layers/src/MapLayersActionIds.ts
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-
-export enum MapLayersSyncUiEventId {
-  /** The display style settings map imagery has changed. */
-  MapImageryChanged = "mapimagerychanged",
-}
--- packages/itwin/test-utilities/src/test-utilities.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
-
-export * from "./test-utilities/IModelUtils.js";
-export * from "./test-utilities/WaitFor.js";
