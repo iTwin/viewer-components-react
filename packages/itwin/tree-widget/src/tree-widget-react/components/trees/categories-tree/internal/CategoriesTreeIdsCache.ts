7b60519b88ecf35cbc4374cccb357d244327e98a
Jonas <100586436+JonasDov@users.noreply.github.com>
2026-01-22 16:59:42 +0200

    Remove unused function

--- packages/itwin/tree-widget/src/tree-widget-react/components/trees/categories-tree/internal/CategoriesTreeIdsCache.ts
+++ packages/itwin/tree-widget/src/tree-widget-react/components/trees/categories-tree/internal/CategoriesTreeIdsCache.ts
@@ -548,10 +548,6 @@ export class CategoriesTreeIdsCache implements Disposable {
     );
   }
 
-  public getCategoriesOfElementModel(modelId: Id64String): Observable<Id64Set | undefined> {
-    return this.getElementModelsCategories().pipe(map((elementModelsCategories) => elementModelsCategories.get(modelId)?.categoryIds));
-  }
-
   public getModelCategoryIds(modelId: Id64String): Observable<Id64Array> {
     return this.getElementModelsCategories().pipe(
       map((elementModelsCategories) => {
