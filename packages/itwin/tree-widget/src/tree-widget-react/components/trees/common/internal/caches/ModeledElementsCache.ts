0d12cd565703571e8bc090a646e980a38f54ea91
JonasDov <100586436+JonasDov@users.noreply.github.com>
2026-01-29 13:47:46 +0200

    [Tree widget]: Add modeled elements cache (#1572)
    * [Tree widget]: Add modeled elements cache
    
    * Update packages/itwin/tree-widget/src/tree-widget-react/components/trees/common/internal/caches/ModeledElementsCache.ts
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Grigas <35135765+grigasp@users.noreply.github.com>

---
+++ packages/itwin/tree-widget/src/tree-widget-react/components/trees/common/internal/caches/ModeledElementsCache.ts
@@ -0,0 +1,121 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
+
+import { defer, EMPTY, from, map, mergeMap, of, reduce, shareReplay, toArray } from "rxjs";
+import { Guid, Id64 } from "@itwin/core-bentley";
+import { CLASS_NAME_Element } from "../ClassNameDefinitions.js";
+import { catchBeSQLiteInterrupts } from "../UseErrorState.js";
+
+import type { Observable } from "rxjs";
+import type { GuidString, Id64Arg, Id64Array, Id64String } from "@itwin/core-bentley";
+import type { LimitingECSqlQueryExecutor } from "@itwin/presentation-hierarchies";
+import type { CategoryId, ElementId, ModelId } from "../Types.js";
+
+interface ModeledElementsCacheProps {
+  queryExecutor: LimitingECSqlQueryExecutor;
+  componentId?: GuidString;
+  elementClassNames: Array<string>;
+  modelClassName: string;
+}
+
+/** @internal */
+export class ModeledElementsCache {
+  #queryExecutor: LimitingECSqlQueryExecutor;
+  #componentId: GuidString;
+  #componentName: string;
+  #elementsClassNames: Array<string>;
+  #modelClassName: string;
+  // ElementId here is also a ModelId, since those elements are sub models.
+  #modeledElementsInfo:
+    | Observable<{ modelWithCategoryModeledElements: Map<ModelId, Map<CategoryId, Set<ElementId>>>; allSubModels: Set<ElementId> }>
+    | undefined;
+
+  constructor(props: ModeledElementsCacheProps) {
+    this.#queryExecutor = props.queryExecutor;
+    this.#componentId = props.componentId ?? Guid.createValue();
+    this.#componentName = "ModeledElementsCache";
+    this.#elementsClassNames = props.elementClassNames;
+    this.#modelClassName = props.modelClassName;
+  }
+
+  private queryModeledElements(): Observable<{
+    modelId: Id64String;
+    modeledElementId: Id64String;
+    categoryId: Id64String;
+  }> {
+    return defer(() => {
+      const query = this.#elementsClassNames
+        .map((elementClassName) => {
+          return `
+            SELECT
+              pe.ECInstanceId modeledElementId,
+              pe.Category.Id categoryId,
+              pe.Model.Id modelId
+            FROM ${this.#modelClassName} m
+            JOIN ${elementClassName} pe ON pe.ECInstanceId = m.ModeledElement.Id
+            WHERE
+              m.IsPrivate = false
+              AND m.ECInstanceId IN (SELECT Model.Id FROM ${this.#elementsClassNames.length > 1 ? CLASS_NAME_Element : elementClassName})
+          `;
+        })
+        .join(" UNION ALL ");
+      return this.#queryExecutor.createQueryReader(
+        { ecsql: query },
+        { rowFormat: "ECSqlPropertyNames", limit: "unbounded", restartToken: `${this.#componentName}/${this.#componentId}/modeled-elements` },
+      );
+    }).pipe(
+      catchBeSQLiteInterrupts,
+      map((row) => {
+        return { modelId: row.modelId, categoryId: row.categoryId, modeledElementId: row.modeledElementId };
+      }),
+    );
+  }
+
+  public getModeledElementsInfo() {
+    this.#modeledElementsInfo ??= this.queryModeledElements().pipe(
+      reduce(
+        (acc, { modelId, categoryId, modeledElementId }) => {
+          let modelEntry = acc.modelWithCategoryModeledElements.get(modelId);
+          if (!modelEntry) {
+            modelEntry = new Map();
+            acc.modelWithCategoryModeledElements.set(modelId, modelEntry);
+          }
+          const categoryEntry = modelEntry.get(categoryId);
+          if (!categoryEntry) {
+            modelEntry.set(categoryId, new Set([modeledElementId]));
+          } else {
+            categoryEntry.add(modeledElementId);
+          }
+          acc.allSubModels.add(modeledElementId);
+          return acc;
+        },
+        { modelWithCategoryModeledElements: new Map<ModelId, Map<CategoryId, Set<ElementId>>>(), allSubModels: new Set<ElementId>() },
+      ),
+      shareReplay(),
+    );
+    return this.#modeledElementsInfo;
+  }
+
+  public hasSubModel(elementId: Id64String): Observable<boolean> {
+    return this.getModeledElementsInfo().pipe(map(({ allSubModels }) => allSubModels.has(elementId)));
+  }
+
+  public getCategoriesModeledElements(modelId: Id64String, categoryIds: Id64Arg): Observable<Id64Array> {
+    return this.getModeledElementsInfo().pipe(
+      mergeMap(({ modelWithCategoryModeledElements }) => {
+        const result = new Array<ElementId>();
+        const categoryMap = modelWithCategoryModeledElements.get(modelId);
+        if (!categoryMap) {
+          return of(result);
+        }
+        return from(Id64.iterable(categoryIds)).pipe(
+          map((categoryId) => categoryMap.get(categoryId)),
+          mergeMap((elementsSet) => (elementsSet ? from(elementsSet) : EMPTY)),
+          toArray(),
+        );
+      }),
+    );
+  }
+}
