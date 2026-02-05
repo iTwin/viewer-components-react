1a11a0e1d1bfcde2d77e261734dc9b05833c9750
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-02-05 11:32:25 +0200

    prettier

--- packages/itwin/property-grid/src/test/hooks/UseDataProvider.test.tsx
+++ packages/itwin/property-grid/src/test/hooks/UseDataProvider.test.tsx
@@ -1,4 +1,3 @@
-;
 /*---------------------------------------------------------------------------------------------
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
@@ -13,16 +12,10 @@ import { useDataProvider } from "../../property-grid-react/hooks/UseDataProvider
 import { TelemetryContextProvider } from "../../property-grid-react/hooks/UseTelemetryContext.js";
 import { createPropertyRecord, createResolvablePromise, renderHook, stubFavoriteProperties, stubPresentation } from "../TestUtils.js";
 
-
-
 import type { PropsWithChildren } from "react";
 import type { PropertyData } from "@itwin/components-react";
 import type { IModelConnection } from "@itwin/core-frontend";
 import type { PerformanceTrackedFeatures } from "../../property-grid-react/hooks/UseTelemetryContext.js";
-
-
-
-
 
 describe("useDataProvider", () => {
   const imodel = {} as IModelConnection;
