abb24773198652ffbf3bcbc50f229a48a4023e6f
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-03-03 18:38:23 +0200

    Remove localization statics

--- packages/itwin/tree-widget/src/tree-widget-react/TreeWidget.ts
+++ packages/itwin/tree-widget/src/tree-widget-react/TreeWidget.ts
@@ -9,7 +9,7 @@ import { createLogger } from "@itwin/presentation-core-interop";
 import { setLogger as setHierarchiesLogger } from "@itwin/presentation-hierarchies";
 import { setLogger as setHierarchiesReactLogger } from "@itwin/presentation-hierarchies-react";
 
-import type { Localization, TranslationOptions } from "@itwin/core-common";
+import type { Localization } from "@itwin/core-common";
 import type { ILogger } from "@itwin/presentation-shared";
 
 /**
@@ -26,7 +26,7 @@ export class TreeWidget {
    * @param i18n - The internationalization service created by the IModelApp.
    * @param logger - The logger to use for logging messages. Defaults to `Logger` from `@itwin/core-bentley`.
    */
-  public static async initialize(i18n?: Localization, logger?: ILogger): Promise<void> {
+  public static async initialize(_i18n?: Localization, logger?: ILogger): Promise<void> {
     if (this.#initialized) {
       return;
     }
@@ -36,18 +36,10 @@ export class TreeWidget {
     TreeWidget.#logger = logger ?? createLogger(Logger);
     setHierarchiesLogger(TreeWidget.#logger);
     setHierarchiesReactLogger(TreeWidget.#logger);
-
-    TreeWidget.#i18n = i18n ?? IModelApp.localization;
-    return TreeWidget.#i18n.registerNamespace(TreeWidget.i18nNamespace);
   }
 
   /** Unregisters the TreeWidget internationalization service namespace */
   public static terminate() {
-    if (TreeWidget.#i18n) {
-      TreeWidget.#i18n.unregisterNamespace(TreeWidget.i18nNamespace);
-      TreeWidget.#i18n = undefined;
-    }
-
     TreeWidget.#logger = undefined;
     setHierarchiesLogger(undefined);
     setHierarchiesReactLogger(undefined);
@@ -61,25 +53,5 @@ export class TreeWidget {
       throw new BentleyError(BentleyStatus.ERROR, "TreeWidget not initialized");
     }
     return TreeWidget.#logger;
-  }
-
-  /** The internationalization service created by the IModelApp. */
-  public static get i18n(): Localization {
-    if (!TreeWidget.#i18n) {
-      throw new BentleyError(BentleyStatus.ERROR, "TreeWidget not initialized");
-    }
-    return TreeWidget.#i18n;
-  }
-
-  /** The internationalization service namespace. */
-  public static get i18nNamespace(): string {
-    return "TreeWidget";
-  }
-
-  /** Calls i18n.translateWithNamespace with the "TreeWidget" namespace. Do NOT include the namespace in the key.
-   */
-  public static translate(key: string, options?: TranslationOptions): string {
-    const stringKey = `${TreeWidget.i18nNamespace}:${key}`;
-    return TreeWidget.i18n.getLocalizedString(stringKey, options);
   }
 }
