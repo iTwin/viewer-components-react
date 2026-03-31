80bc8793d388840bd025adfc4be04bc45957ac96
Jonas <100586436+JonasDov@users.noreply.github.com>
2026-03-31 13:22:28 +0300

    Add IS_REACT_ACT_ENVIRONMENT

--- packages/itwin/property-grid/src/test/setup.ts
+++ packages/itwin/property-grid/src/test/setup.ts
@@ -12,6 +12,7 @@ const originalConsoleError = console.error;
 let caughtLogsWhichFailTest = new Array<string>();
 
 beforeAll(() => {
+  getGlobalThis().IS_REACT_ACT_ENVIRONMENT = true;
   console.error = (message?: any, ...optionalParams: any[]) => {
     if (typeof message === "string") {
       const caughtMessage = logsWhichFailTest.find((log) => message.includes(log));
@@ -40,4 +41,21 @@ afterEach(() => {
 
 afterAll(() => {
   console.error = originalConsoleError;
+  delete getGlobalThis().IS_REACT_ACT_ENVIRONMENT;
 });
+
+function getGlobalThis(): typeof globalThis & { IS_REACT_ACT_ENVIRONMENT?: boolean } {
+  if (typeof globalThis !== "undefined") {
+    return globalThis;
+  }
+  if (typeof self !== "undefined") {
+    return self;
+  }
+  if (typeof window !== "undefined") {
+    return window;
+  }
+  if (typeof global !== "undefined") {
+    return global;
+  }
+  throw new Error("unable to locate global object");
+}
