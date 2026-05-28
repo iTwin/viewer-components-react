ce4046bdeb554e4769be6c67239eac4e2a72c233
Saulius.Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-05-28 16:13:55 +0300

    Remove abandoned packages

--- packages/archive/breakdown-trees/src/Views/OptionItemHandlers/index.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
-export * from "./LabelHandler";
-export * from "./OptionItemHandler";
-export * from "./TopViewHandler";
-export * from "./ClipAtSpacesHandler";
-export * from "./GenericOptionItemHandler";
--- packages/archive/infrastructure-iot/src/enums/imodel/IModelMarkerStyleEnum.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
-
-export enum IModelMarkerStyle {
-  OVERLAY = "overlay",
-  ELEMENT = "element"
-}
