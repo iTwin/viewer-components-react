9da9db520af6219c579522f709276b39083c5c98
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-06-09 14:19:57 +0300

    Remove abandoned packages (#1700)

--- packages/archive/imodel-select/src/imodel-select-react.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
-export { IModelSelect } from "./IModelSelect";
-export { IModelInfo } from "./api/IModelInfoService";
-export { ProjectInfo } from "./api/ProjectInfoService";
-export { IModelSelector } from "./components/IModelSelector";
-export { ProjectSelector } from "./components/ProjectSelector";
--- packages/archive/infrastructure-iot/src/models/AppConfigInterface.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
-* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
-* See LICENSE.md in the project root for license terms and full copyright notice.
-*--------------------------------------------------------------------------------------------*/
-
-export interface AppConfig {
-  environment?: "production" | "development";
-  enableLogging?: boolean;
-}
