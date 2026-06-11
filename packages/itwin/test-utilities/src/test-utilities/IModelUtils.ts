854e79c5bb7ccc0a7087361ebc8f938834af8f78
Jonas <100586436+JonasDov@users.noreply.github.com>
2026-06-11 09:51:36 +0300

    Actual revert

--- packages/itwin/test-utilities/src/test-utilities/IModelUtils.ts
+++ packages/itwin/test-utilities/src/test-utilities/IModelUtils.ts
@@ -240,9 +240,7 @@ export function insertPhysicalPartition(
 }
 
 export function insertPhysicalSubModel(
-  props: BaseInstanceInsertProps & { modeledElementId: Id64String; relationshipName?: EC.FullClassName } & Partial<
-      Omit<GeometricModel3dProps, "id" | "modeledElement" | "parentModel">
-    >,
+  props: BaseInstanceInsertProps & { modeledElementId: Id64String } & Partial<Omit<GeometricModel3dProps, "id" | "modeledElement" | "parentModel">>,
 ) {
   return insertSubModel({
     classFullName: `BisCore${props.fullClassNameSeparator ?? "."}PhysicalModel`,
@@ -268,9 +266,7 @@ export function insertDrawingPartition(
 }
 
 export function insertDrawingSubModel(
-  props: BaseInstanceInsertProps & { modeledElementId: Id64String; relationshipName?: EC.FullClassName } & Partial<
-      Omit<GeometricModel2dProps, "id" | "modeledElement" | "parentModel">
-    >,
+  props: BaseInstanceInsertProps & { modeledElementId: Id64String } & Partial<Omit<GeometricModel2dProps, "id" | "modeledElement" | "parentModel">>,
 ) {
   return insertSubModel({
     classFullName: `BisCore${props.fullClassNameSeparator ?? "."}DrawingModel`,
