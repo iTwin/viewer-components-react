ee26026a14b55008aee8d9a95c8075f4431e336f
Maxime Brassard <maxime.brassard@bentley.com>
2026-07-16 12:04:23 -0400

    inverted

--- packages/itwin/measure-tools/src/api/MeasurementTool.ts
+++ packages/itwin/measure-tools/src/api/MeasurementTool.ts
@@ -221,7 +221,7 @@ export abstract class MeasurementToolBase<
     return true;
   }
 
-  protected get allowedDrawingTypes(): SheetMeasurementHelper.DrawingType[] {
+  protected get forbiddenDrawingTypes(): SheetMeasurementHelper.DrawingType[] {
     return [];
   }
 
@@ -361,7 +361,7 @@ export abstract class MeasurementToolBase<
     if (!this._enableSheetMeasurements) {
       return defaultToolTip(hit);
     } else {
-      return SheetMeasurementHelper.getSheetToolTipText(hit, this.allowedDrawingTypes, defaultToolTip);
+      return SheetMeasurementHelper.getSheetToolTipText(hit, this.forbiddenDrawingTypes, defaultToolTip);
     }
   }
 
