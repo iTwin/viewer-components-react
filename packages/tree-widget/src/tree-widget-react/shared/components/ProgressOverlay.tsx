cddf250cba731bb5cb80d8bac5d005688b09337b
MartynasStrazdas <43886789+MartynasStrazdas@users.noreply.github.com>
2026-08-06 15:58:33 +0300

    update e2e snapshots and readd visuallyHidden

--- packages/tree-widget/src/tree-widget-react/shared/components/ProgressOverlay.tsx
+++ packages/tree-widget/src/tree-widget-react/shared/components/ProgressOverlay.tsx
@@ -6,6 +6,7 @@
 import "./ProgressOverlay.css";
 
 import { LinearProgress } from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
 import { useTranslation } from "./LocalizationContext.js";
 
 /** @internal */
@@ -13,7 +14,10 @@ export function ProgressOverlay() {
   const translate = useTranslation();
   return (
     <div className="tw-progress-overlay-container">
-      <LinearProgress aria-label={translate("loading.search")} color={"primary"} />
+      <LinearProgress aria-labelledby={"tw-progress-bar"} color={"primary"} />
+      <span id={"tw-progress-bar"} style={visuallyHidden}>
+        {translate("loading.search")}
+      </span>
       <div className="tw-progress-overlay-backdrop" />
     </div>
   );
--- packages/tree-widget/src/tree-widget-react/shared/components/SkeletonTree.tsx
+++ packages/tree-widget/src/tree-widget-react/shared/components/SkeletonTree.tsx
@@ -6,17 +6,23 @@
 import "./SkeletonTree.css";
 
 import { Skeleton } from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
+import { useTranslation } from "./LocalizationContext.js";
 
 /**
  * Represents the loaded tree content before it finishes loading.
  * @beta
  */
 export function SkeletonTree() {
+  const translate = useTranslation();
   return (
     <div className="tw-skeleton-container" role="status">
       {Array.from({ length: 20 }, (_, index) => (
         <SkeletonRow key={index} />
       ))}
+      <span id={"tw-progress-bar"} style={visuallyHidden}>
+        {translate("loading.skeleton")}
+      </span>
     </div>
   );
 }
