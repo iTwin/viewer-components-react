60c39e9db384c0f1d3a7585b40a76c66ec92e022
Saulius Skliutas <24278440+saskliutas@users.noreply.github.com>
2026-08-21 13:41:53 +0300

    Apply suggestions from code review
    Co-authored-by: JonasDov <100586436+JonasDov@users.noreply.github.com>

--- packages/tree-widget/src/test/components/TreeNodeVisibilityButton.test.tsx
+++ packages/tree-widget/src/test/components/TreeNodeVisibilityButton.test.tsx
@@ -1,5 +1,6 @@
 /*---------------------------------------------------------------------------------------------
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
 
 import { describe, expect, it, vi } from "vitest";
